d43f0458ab567449244195b80605a14e5be859d1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-02-11 22:17:26 +0100

    create validation error builder

--- lib/infrastructure/service/validator/has-to-be-one-command-dto-validation.service.ts
+++ lib/infrastructure/service/validator/has-to-be-one-command-dto-validation.service.ts
@@ -3,17 +3,16 @@ import { LoggerModeEnum } from "../../enum/logger-mode.enum";
 import { ParameterTypeEnum } from "../../enum/parameter-type.enum";
 import { LoggerModel } from "../../model/logger.model";
 import { ParameterDtoModel } from "../../model/parameter-dto.model";
+import { ValidationErrorBuilderService } from "../builder/validation-error-builder.service";
 
 @singleton()
 export class HasToBeOneCommandDtoValidationService {
+  constructor(private readonly validationErrorBuilder: ValidationErrorBuilderService) {}
+  
   run(parameterDto: ParameterDtoModel): LoggerModel | true {
     const commands = parameterDto.parameters
       .filter(parameter => parameter.type === ParameterTypeEnum.command);
     if (commands.length === 1) return true;
-    return {
-      mode: LoggerModeEnum.error,
-      message: "A parameter validation error occurred!",
-      newLine: false
-    };
+    return this.validationErrorBuilder.build(parameterDto, "A parameter validation error occurred!");
   }
 }
