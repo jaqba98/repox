e29dae97223839e492dc2613d7f6564a476ebf74
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-03-26 22:50:49 +0200

    feature: add first simply test of read param domain

--- test/app-service/read-param-domain-app.test.ts
+++ test/app-service/read-param-domain-app.test.ts
@@ -1,3 +1,55 @@
+import { container } from "tsyringe";
+import {
+  ReadParamDomainAppService
+} from "../../src/app-service/read-param-domain-app.service";
+import {
+  ParamDtoModel
+} from "../../src/infra/model/param-dto/param-dto.model";
+import { ParamTypeEnum } from "../../src/infra/enum/param-type.enum";
+import {
+  ParamDomainValidationModel
+} from "../../src/model/param-domain/param-domain-validation.model";
+import { ProgramEnum } from "../../src/enum/program.enum";
+import { CommandEnum } from "../../src/enum/command.enum";
+
 test("test", () => {
-  expect(true).toBeTruthy();
-})
+  const paramDto: ParamDtoModel = {
+    params: [
+      {
+        paramBaseValue: "node.exe",
+        paramIndex: 0,
+        paramType: ParamTypeEnum.executor,
+        paramHasValue: false,
+        paramName: "node.exe",
+        paramValues: []
+      },
+      {
+        paramBaseValue: "repox.js",
+        paramIndex: 1,
+        paramType: ParamTypeEnum.application,
+        paramHasValue: false,
+        paramName: "repox.js",
+        paramValues: []
+      }
+    ]
+  };
+  const service = container.resolve(ReadParamDomainAppService);
+  expect(service.build(paramDto)).toEqual<ParamDomainValidationModel>({
+    isError: false,
+    wrongParamIndexes: [],
+    errors: [],
+    tips: [],
+    paramDomain: {
+      program: {
+        name: ProgramEnum.default,
+        index: 1,
+        args: []
+      },
+      command: {
+        name: CommandEnum.default,
+        index: 2,
+        args: []
+      }
+    }
+  });
+});
