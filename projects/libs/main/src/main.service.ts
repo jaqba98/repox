ea560125544bedbe1e7a30c535d12398d88da90b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-05-15 17:51:29 +0200

    chore: create the dependency diagram

--- projects/libs/main/src/main.service.ts
+++ projects/libs/main/src/main.service.ts
@@ -5,7 +5,7 @@ import {
 } from "@lib/parameter";
 import { container, singleton } from "tsyringe";
 import { LoggerParamErrorAppService } from "@lib/logger";
-import { SelectProgramAppService } from "@lib/select-program";
+import { SelectProgramAppService } from "projects/libs/launcher";
 
 @singleton()
 /**
