16329ab40cd4f52d568d919b96c46e8e175de2d0
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-06-04 13:05:00 +0200

    feat: add setItem and getItem to base store

--- projects/libs/core/src/lib/base-store/__tests__/base-store.test.ts
+++ projects/libs/core/src/lib/base-store/__tests__/base-store.test.ts
@@ -44,4 +44,33 @@ describe("BaseStoreService", () => {
       "The store is undefined!"
     );
   });
+
+  test("Should be correct 1", () => {
+    const service = container.resolve(BaseStoreService<ExampleModel>);
+    service.setData({
+      prop1: "hello",
+      prop2: true,
+      prop3: ["a", "b", "c"],
+      prop4: {
+        prop41: false,
+        prop42: ["A", "B", "C"]
+      }
+    });
+    service.setItem<string[]>(["prop4", "prop42"], ["AA", "BB", "CC"]);
+    expect(service.getItem<string[]>(["prop4", "prop42"])).toEqual(["AA", "BB", "CC"]);
+  });
+
+  test("Should be correct", () => {
+    const service = container.resolve(BaseStoreService<ExampleModel>);
+    service.setData({
+      prop1: "hello",
+      prop2: true,
+      prop3: ["a", "b", "c"],
+      prop4: {
+        prop41: false,
+        prop42: ["A", "B", "C"]
+      }
+    });
+    expect(service.getItem<boolean>(["prop4", "prop41"])).toBeFalsy();
+  });
 });
--- projects/libs/core/src/lib/base-store/base-store.service.ts
+++ projects/libs/core/src/lib/base-store/base-store.service.ts
@@ -22,4 +22,47 @@ export class BaseStoreService<TData> {
     }
     return this.data;
   }
+
+  setItem<TItem>(path: string[], value: TItem): void {
+    this.setCurrentItem<TItem>(this.data, path, value);
+  }
+
+  getItem<TItem>(path: string[]): TItem {
+    return this.getCurrentItem<TItem>(this.data, path);
+  }
+
+  private setCurrentItem<TItem>(
+    currentData: any, paths: string[], value: TItem
+  ): void {
+    const [firstPath, ...remainingPaths] = paths;
+    if (firstPath === undefined) {
+      throw new Error("The path element is undefined!");
+    }
+    if (remainingPaths.length === 0) {
+      currentData[firstPath] = value;
+      return;
+    }
+    if (currentData[firstPath] === undefined) {
+      throw new Error("The data element is undefined!");
+    }
+    this.setCurrentItem<TItem>(
+      currentData[firstPath], remainingPaths, value
+    );
+  }
+
+  private getCurrentItem<TItem>(
+    currentData: any, paths: string[]
+  ): TItem {
+    const [firstPath, ...remainingPaths] = paths;
+    if (firstPath === undefined) {
+      throw new Error("The path element is undefined!");
+    }
+    const dataElement = currentData[firstPath];
+    if (dataElement === undefined) {
+      throw new Error("The data element is undefined!");
+    }
+    return remainingPaths.length === 0
+      ? dataElement
+      : this.getCurrentItem<TItem>(dataElement, remainingPaths);
+  }
 }
