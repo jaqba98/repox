fe6d9976032794be9f51ba0ac2c0b0e16451b6ae
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-06-18 17:10:55 +0200

    refactor: fix the system verification

--- projects/libs/program/src/lib/app-service/system-verification-app.service.ts
+++ projects/libs/program/src/lib/app-service/system-verification-app.service.ts
@@ -1,21 +1,50 @@
+// Refactored file
 import { singleton } from "tsyringe";
 import {
   ProgramInstalledService
 } from "../infrastructure/program-installed.service";
+import { SimpleMessageAppService } from "@lib/logger";
 
 @singleton()
 /**
- * The app service is responsible for system verification.
+ * The app service is responsible for checking
+ * whether the system is correct and everything is installed.
  */
 export class SystemVerificationAppService {
   constructor(
-    private readonly programInstalled: ProgramInstalledService
+    private readonly programInstalled: ProgramInstalledService,
+    private readonly simple: SimpleMessageAppService
   ) {
   }
 
   checkSystem(): boolean {
-    if (!this.programInstalled.check("git")) return false;
-    if (!this.programInstalled.check("node")) return false;
-    return this.programInstalled.check("npm");
+    if (!this.programInstalled.check("git")) {
+      this.simple.writeError(
+        "The GIT is not installed on the computer", 0, false, true
+      );
+      this.simple.writeWarning(
+        "Install GIT on the computer", 0, false, true
+      );
+      return false;
+    }
+    if (!this.programInstalled.check("node")) {
+      this.simple.writeError(
+        "The NODE.JS is not installed on the computer", 0, false, true
+      );
+      this.simple.writeWarning(
+        "Install NODE.JS on the computer", 0, false, true
+      );
+      return false;
+    }
+    if (!this.programInstalled.check("npm")) {
+      this.simple.writeError(
+        "The NPM is not installed on the computer", 0, false, true
+      );
+      this.simple.writeWarning(
+        "Install NPM on the computer", 0, false, true
+      );
+      return false;
+    }
+    return true;
   }
 }
--- projects/libs/program/src/lib/infrastructure/program-installed.service.ts
+++ projects/libs/program/src/lib/infrastructure/program-installed.service.ts
@@ -1,27 +1,14 @@
+// Refactored file
 import { singleton } from "tsyringe";
 import { sync } from "command-exists";
-import { SimpleMessageAppService } from "@lib/logger";
 
 @singleton()
 /**
- * The service is responsible for verify whether program
- * is installed.
+ * The service is responsible for checking
+ * whether the given program is installed on system.
  */
 export class ProgramInstalledService {
-  constructor(
-    private readonly simpleMessage: SimpleMessageAppService
-  ) {
-  }
-
   check(program: string): boolean {
-    if (sync(program)) return true;
-    this.simpleMessage.writeError(
-      `Program ${program} is not installed on the system!`, 0, false,
-      true
-    );
-    this.simpleMessage.writeWarning(
-      `You have to install ${program} on your device`, 0, false, true
-    );
-    return false;
+    return sync(program);
   }
 }
