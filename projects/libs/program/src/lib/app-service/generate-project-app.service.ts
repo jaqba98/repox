5ff4de5c01fb67f47f822523930f95799ddd8f88
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-06-21 11:41:06 +0200

    feat: add two different ways to build project

--- projects/libs/program/src/lib/app-service/generate-project-app.service.ts
+++ projects/libs/program/src/lib/app-service/generate-project-app.service.ts
@@ -19,10 +19,7 @@ import { PathUtilsService, WriteFileService } from "@lib/utils";
 import {
   CreateEmptyFileService
 } from "../infrastructure/create-empty-file.service";
-import {
-  BuildProjectSchemeAppService, ProjectSchemeEnum,
-  ProjectTypeEnum
-} from "@lib/project";
+import { ProjectSchemeEnum, ProjectTypeEnum } from "@lib/project";
 import {
   GIT_KEEP,
   INDEX_FILE,
@@ -45,8 +42,7 @@ export class GenerateProjectAppService {
     private readonly runCommand: RunCommandService,
     private readonly writeFile: WriteFileService,
     private readonly buildDefaultDomain: BuildDefaultDomainAppService,
-    private readonly createEmptyFile: CreateEmptyFileService,
-    private readonly buildProjectScheme: BuildProjectSchemeAppService
+    private readonly createEmptyFile: CreateEmptyFileService
   ) {
   }
 
@@ -57,36 +53,32 @@ export class GenerateProjectAppService {
     alias: string,
     scheme: ProjectSchemeEnum
   ): boolean {
-    if (!this.generateConfig(name, type, path, alias, scheme)) {
-      return false;
-    }
-    return this.generateFiles(path);
-  }
-
-  private generateConfig(
-    name: string,
-    type: ProjectTypeEnum,
-    path: string,
-    alias: string,
-    scheme: ProjectSchemeEnum
-  ): boolean {
     // Display generate project config header
     this.simple.writePlain("Generate project configuration");
     // Add a new project to the repox.json file
     this.domainConfigStore.addProject(name, type, path, scheme);
-    // Add a new alias to the tsconfig.json file
-    const aliasPath = this.pathUtils.createPath(
-      [path, SRC, INDEX_FILE]
-    );
-    this.domainConfigStore.addAlias(alias, aliasPath);
-    // Save the domain config on the disc
-    this.domainConfigStore.saveConfig();
-    return true;
+    switch (scheme) {
+      case ProjectSchemeEnum.typescript:
+        // Add a new alias to the tsconfig.json file
+        const aliasPath = this.pathUtils.createPath(
+          [path, SRC, INDEX_FILE]
+        );
+        this.domainConfigStore.addAlias(alias, aliasPath);
+        // Save the domain config on the disc
+        this.domainConfigStore.saveConfig();
+        return this.generateTypescriptFiles(path);
+      case ProjectSchemeEnum.staticPage:
+        // Save the domain config on the disc
+        this.domainConfigStore.saveConfig();
+        return this.generateStaticPageFiles(path);
+      default:
+        throw new Error("Not supported scheme!");
+    }
   }
 
-  private generateFiles(path: string): boolean {
+  private generateTypescriptFiles(path: string): boolean {
     // Display generate project files header
-    this.simple.writePlain("Generate project files");
+    this.simple.writePlain("Generate typescript project files");
     // Create the project file and go inside
     this.createFolder.create(path);
     this.changePath.change(path);
@@ -102,6 +94,15 @@ export class GenerateProjectAppService {
     this.createEmptyFile.create(`${SRC}/${INDEX_FILE}`);
     this.createFolder.create(`${SRC}/${LIB}`);
     this.createEmptyFile.create(`${SRC}/${LIB}/${GIT_KEEP}`);
+    return true;
+  }
+
+  private generateStaticPageFiles(path: string): boolean {
+    // Display generate project files header
+    this.simple.writePlain("Generate static page project files");
+    // Create the project file and go inside
+    this.createFolder.create(path);
+    this.changePath.change(path);
     return true;
   }
 }
