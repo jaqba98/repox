0ca1b337fd361aaed1aa2b9e168fa4d77a3f9253
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-07-05 15:04:20 +0200

    chore: comment the base build name method

--- projects/libs/param-domain/src/lib/dom-service/builder/build-param-name.service.ts
+++ projects/libs/param-domain/src/lib/dom-service/builder/build-param-name.service.ts
@@ -1,6 +1,5 @@
 import { singleton } from "tsyringe";
 import { KeyValueModel } from "@lib/model";
-import { EMPTY_STRING } from "@lib/const";
 import { BaseParamTypeEnum } from "../../enum/base-param-type.enum";
 import { ParamTypeEnum } from "@lib/param-dto";
 
@@ -49,47 +48,48 @@ export class BuildParamNameService {
     throw new Error(`Not supported param type: ${paramType}`);
   }
 
-  // todo: refactor the method
   private baseBuildName(
     baseName: string,
     enums: Array<KeyValueModel>,
     aliasEnums: Array<KeyValueModel>
   ): string {
-    // Verification the enums and alias enums
-    if (!enums.some(enumItem => enumItem.key === "default")) {
-      throw new Error("Not defined default key in the enum");
-    }
-    if (!enums.some(enumItem => enumItem.key === "unknown")) {
-      throw new Error("Not defined unknown key in the enum");
-    }
-    // Return empty string then baseName is empty string
-    if (baseName === EMPTY_STRING) {
-      const defaultEnum = enums.find(enumItem =>
-        enumItem.key === "default" &&
-        enumItem.value === EMPTY_STRING
-      );
-      if (!defaultEnum) {
-        throw new Error("Not defined default key in the enum");
-      }
-      return defaultEnum.key;
-    }
-    // Find name by alias enums
-    const alias = aliasEnums.find(alias => alias.value === baseName);
-    if (alias) {
-      const mainEnum = enums.find(
-        enumItem => enumItem.key === alias.key
-      );
-      if (!mainEnum) {
-        throw new Error("Not defined main enum for given alias enum");
-      }
-      return mainEnum.key;
-    }
-    // Find name by enums
-    const main = enums.find(enumItem => enumItem.value === baseName);
-    if (main) {
-      return main.key;
-    }
-    return "unknown";
+    // todo: I am here
+    return "";
+    // // Verification the enums and alias enums
+    // if (!enums.some(enumItem => enumItem.key === "default")) {
+    //   throw new Error("Not defined default key in the enum");
+    // }
+    // if (!enums.some(enumItem => enumItem.key === "unknown")) {
+    //   throw new Error("Not defined unknown key in the enum");
+    // }
+    // // Return empty string then baseName is empty string
+    // if (baseName === EMPTY_STRING) {
+    //   const defaultEnum = enums.find(enumItem =>
+    //     enumItem.key === "default" &&
+    //     enumItem.value === EMPTY_STRING
+    //   );
+    //   if (!defaultEnum) {
+    //     throw new Error("Not defined default key in the enum");
+    //   }
+    //   return defaultEnum.key;
+    // }
+    // // Find name by alias enums
+    // const alias = aliasEnums.find(alias => alias.value === baseName);
+    // if (alias) {
+    //   const mainEnum = enums.find(
+    //     enumItem => enumItem.key === alias.key
+    //   );
+    //   if (!mainEnum) {
+    //     throw new Error("Not defined main enum for given alias enum");
+    //   }
+    //   return mainEnum.key;
+    // }
+    // // Find name by enums
+    // const main = enums.find(enumItem => enumItem.value === baseName);
+    // if (main) {
+    //   return main.key;
+    // }
+    // return "unknown";
   }
 }
 
