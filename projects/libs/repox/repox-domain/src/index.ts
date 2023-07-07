cdf7f7fe84baf58369b2b1d8f86deb6943d3185d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-07-07 14:15:19 +0200

    refactor: move program and command models to the repox-domain

--- projects/libs/repox/repox-domain/src/index.ts
+++ projects/libs/repox/repox-domain/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./lib/enum/program-repox.enum";
 export * from "./lib/enum/command-repox.enum";
 export * from "./lib/enum/argument-repox.enum";
+export * from "./lib/model/command-arg-repox.model";
+export * from "./lib/model/program-arg-repox.model";
 export * from "./lib/service/repox-launch-program.service";
---
+++ projects/libs/repox/repox-domain/src/lib/model/program-arg-repox.model.ts
@@ -0,0 +1,14 @@
+/**
+ * The complete list of models for repox program arguments.
+ */
+
+export interface EmptyProgramArgRepoxModel {
+}
+
+export interface DefaultDefaultProgramArgRepoxModel {
+  version: boolean;
+}
+
+export type ProgramArgRepoxModel =
+  EmptyProgramArgRepoxModel |
+  DefaultDefaultProgramArgRepoxModel;
