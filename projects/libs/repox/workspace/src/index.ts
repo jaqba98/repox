63bba8fc9cd7c5f6c8fcf1e83f28e6a221a5dfc9
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-07-07 11:22:35 +0200

    chore: fix no existed export

--- projects/libs/repox/workspace/src/index.ts
+++ projects/libs/repox/workspace/src/index.ts
@@ -2,7 +2,6 @@ export * from "./lib/app-service/project-app.service";
 export * from "./lib/dom-service/store/domain-store.service";
 export * from "./lib/enum/project/project-scheme.enum";
 export * from "./lib/enum/config/config-file.enum";
-export * from "./lib/const/workspace.const";
 export * from "./lib/model/program-arg-domain.model";
 export * from "./lib/model/command-arg-domain.model";
 export * from "./lib/enum/param-program/argument.enum";
