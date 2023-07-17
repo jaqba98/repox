60cd35ece5a92a24748e4e1395b00983f4b4a265
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-07-17 15:51:32 +0200

    feat: add load domain ws to build project

--- projects/libs/repox/repox-program/src/lib/step/build-project-step.service.ts
+++ projects/libs/repox/repox-program/src/lib/step/build-project-step.service.ts
@@ -8,6 +8,19 @@ import {
   BuildProjectRepoxCommandModel,
   EmptyRepoxProgramModel
 } from "@lib/repox-domain";
+import { ProgramSystemEnum } from "../enum/program-system.enum";
+import {
+  ProgramInstalledAppService
+} from "../app-service/program-installed-app.service";
+import {
+  GoToProjectRootAppService
+} from "../app-service/go-to-project-root-app.service";
+import {
+  LoadWsDtoAppService
+} from "../app-service/load-ws-dto-app.service";
+import {
+  LoadWsDomainAppService
+} from "../app-service/load-ws-domain-app.service";
 
 @singleton()
 /**
@@ -16,7 +29,11 @@ import {
 export class BuildProjectStepService {
   constructor(
     private readonly simpleMessage: SimpleMessageAppService,
-    private readonly newline: NewlineAppService
+    private readonly newline: NewlineAppService,
+    private readonly programInstalled: ProgramInstalledAppService,
+    private readonly goToProjectRoot: GoToProjectRootAppService,
+    private readonly loadWsDto: LoadWsDtoAppService,
+    private readonly loadWsDomain: LoadWsDomainAppService,
   ) {
   }
 
@@ -26,6 +43,13 @@ export class BuildProjectStepService {
   ): void {
     this.simpleMessage.writeInfo("Build project", REPOX_LOGO);
     this.newline.writeNewline();
+    if (!this.programInstalled.run(ProgramSystemEnum.git)) return;
+    if (!this.programInstalled.run(ProgramSystemEnum.node)) return;
+    if (!this.programInstalled.run(ProgramSystemEnum.npm)) return;
+    if (!this.goToProjectRoot.run()) return;
+    if (!this.loadWsDto.run()) return;
+    if (!this.loadWsDomain.run()) return;
+    console.log("Hello");
   }
 }
 // todo: refactor
