6490edb13901be09af5790b30f77ab1cdb8c02d8
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-07-21 11:13:44 +0200

    feat: add menu options

--- projects/apps/repox-website/src/script.js
+++ projects/apps/repox-website/src/script.js
@@ -1,8 +1,6 @@
-const hamburger = document.getElementById("hamburger");
+const hamburger = document.getElementsByClassName("hamburger");
 const menu = document.getElementsByClassName("menu");
 
-hamburger.addEventListener("click", () => {
-    for (let i = 0; i < menu.length; i++) {
-        menu[i].classList.toggle("menu-open");
-    }
+hamburger[0].addEventListener("click", () => {
+    menu[0].classList.toggle("menu-open");
 });
