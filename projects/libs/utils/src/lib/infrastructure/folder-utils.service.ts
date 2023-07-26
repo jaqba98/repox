fb6bf00c240853c6e4e2ff0cc8cd835f896bacf1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-07-26 09:32:52 +0200

    fix: inject the dep error

--- projects/libs/utils/src/lib/infrastructure/folder-utils.service.ts
+++ projects/libs/utils/src/lib/infrastructure/folder-utils.service.ts
@@ -2,20 +2,18 @@ import { singleton } from "tsyringe";
 import { mkdirSync } from "fs";
 import { EMPTY_STRING } from "@lib/const";
 import { FileUtilsService } from "./file-utils.service";
+import { extname } from "path";
 
 @singleton()
 /**
  * The service contains a group of utils to file manage.
  */
 export class FolderUtilsService {
-  constructor(private readonly fileUtils: FileUtilsService) {
-  }
-
   createFolder(folderPath: string): void {
     mkdirSync(folderPath, { recursive: true });
   }
 
   isFolder(path: string): boolean {
-    return this.fileUtils.getFileExtname(path) === EMPTY_STRING;
+    return extname(path) === EMPTY_STRING;
   }
 }
