c78770d39096350402e4fb0158f4b7290a11ecc1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-07-27 10:03:08 +0200

    feat: add header section

--- projects/apps/repox-website/src/scripts/downloads.js
+++ projects/apps/repox-website/src/scripts/downloads.js
@@ -2,7 +2,7 @@
 const weeklyDownloads = document.getElementById("weekly-downloads")
 
 // Get weekly downloads
-fetch("https://api.npmjs.org/downloads/point/last-week/repox")
+fetch("https://api.npmjs.org/downloads/range/2010-01-01:2030-01-01/repox")
     .then((response) => response.json())
     .then((data) => {
         weeklyDownloads.innerText = `${data.downloads} +`;
