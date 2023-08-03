cd91c901ab0754c1f6ab8aad323b2ee8834352c7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-08-03 12:45:39 +0200

    feat: add lint all projects

---
+++ projects/libs/repox/repox-program/src/lib/app-service/lint-projects-app.service.ts
@@ -0,0 +1,31 @@
+import { singleton } from "tsyringe";
+import { SimpleMessageAppService } from "@lib/logger";
+import { WsDomainStoreService } from "@lib/repox-workspace";
+import { RunCommandUtilsService } from "@lib/utils";
+
+@singleton()
+/**
+ * The app service is responsible for checking
+ * whether a given project not exist.
+ */
+export class LintProjectsAppService {
+  constructor(
+    private readonly simpleMessage: SimpleMessageAppService,
+    private readonly wsDomainStore: WsDomainStoreService,
+    private readonly runCommandUtils: RunCommandUtilsService
+  ) {
+  }
+
+  run(): boolean {
+    this.simpleMessage.writePlain("Lint projects");
+    const projects = Object.values(
+      this.wsDomainStore.getWsDomain().projects
+    );
+    for(const project of projects) {
+      this.runCommandUtils.runNpxCommand(
+        `eslint ${project.src}/**/*.ts -c .eslintrc.json`
+      )
+    }
+    return true;
+  }
+}
--- projects/libs/repox/repox-program/src/lib/step/lint-project-step.service.ts
+++ projects/libs/repox/repox-program/src/lib/step/lint-project-step.service.ts
@@ -8,6 +8,19 @@ import {
   type EmptyRepoxProgramModel
 } from "@lib/repox-domain";
 import { REPOX_LOGO } from "@lib/repox-const";
+import {
+  AllProgramInstalledService,
+  GoToProjectRootAppService
+} from "@lib/program-step";
+import {
+  LoadWsDtoAppService
+} from "../app-service/load-ws-dto-app.service";
+import {
+  LoadWsDomainAppService
+} from "../app-service/load-ws-domain-app.service";
+import {
+  LintProjectsAppService
+} from "../app-service/lint-projects-app.service";
 
 @singleton()
 /**
@@ -17,15 +30,11 @@ export class LintProjectStepService {
   constructor (
     private readonly simpleMessage: SimpleMessageAppService,
     private readonly newline: NewlineAppService,
-    // private readonly allProgramInstalled: AllProgramInstalledService,
-    // private readonly goToProjectRoot: GoToProjectRootAppService,
-    // private readonly loadWsDto: LoadWsDtoAppService,
-    // private readonly loadWsDomain: LoadWsDomainAppService,
-    // private readonly projectExist: ProjectExistAppService,
-    // private readonly wsDomainStore: WsDomainStoreService,
-    // private readonly folderExist: FolderExistAppService,
-    // private readonly changePath: ChangePathAppService,
-    // private readonly npmPublish: NpmPublishAppService
+    private readonly allProgramInstalled: AllProgramInstalledService,
+    private readonly goToProjectRoot: GoToProjectRootAppService,
+    private readonly loadWsDto: LoadWsDtoAppService,
+    private readonly loadWsDomain: LoadWsDomainAppService,
+    private readonly lintProjectsApp: LintProjectsAppService
   ) {
   }
 
@@ -35,17 +44,11 @@ export class LintProjectStepService {
   ): void {
     this.simpleMessage.writeInfo("Lint project", REPOX_LOGO);
     this.newline.writeNewline();
-    // if (!this.allProgramInstalled.run()) return;
-    // if (!this.goToProjectRoot.run()) return;
-    // if (!this.loadWsDto.run()) return;
-    // if (!this.loadWsDomain.run()) return;
-    // const { projectName } = commandModel;
-    // if (!this.projectExist.run(projectName)) return;
-    // const project = this.wsDomainStore.getProject(projectName);
-    // const output = project?.build.output ?? EMPTY_STRING;
-    // if (!this.folderExist.run(output)) return;
-    // if (!this.changePath.run(output)) return;
-    // if (!this.npmPublish.run()) return;
+    if (!this.allProgramInstalled.run()) return;
+    if (!this.goToProjectRoot.run()) return;
+    if (!this.loadWsDto.run()) return;
+    if (!this.loadWsDomain.run()) return;
+    if (!this.lintProjectsApp.run()) return;
     this.newline.writeNewline();
     this.simpleMessage.writeSuccess(
       "Project linted successfully!"
