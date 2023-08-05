26f237e37ceeb8880692669362d076c14f5a5ab5
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-08-05 11:16:36 +0200

    fix: fix eslint issues inside jest config file in root workspace

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,10 @@ import type { Config } from "jest";
 
 const config: Config = {
   clearMocks: true,
-  coverageProvider: "v8",
-  preset: "ts-jest",
-  setupFilesAfterEnv: ["core-js/features/reflect"],
-  testEnvironment: "jest-environment-node"
+  coverageProvider: `v8`,
+  preset: `ts-jest`,
+  setupFilesAfterEnv: [`core-js/features/reflect`],
+  testEnvironment: `jest-environment-node`
 };
 
 export default config;
