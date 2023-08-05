028211f1d45ab1ec7f9efba9894eaf1669c1b1d4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-08-05 08:51:55 +0200

    feat: add basic html conver test

--- projects/libs/htmlpro/htmlpro-program/src/lib/dom-service/converter/html-convert.test.ts
+++ projects/libs/htmlpro/htmlpro-program/src/lib/dom-service/converter/html-convert.test.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 import { HtmlConverterService } from "./html-converter.service";
+import type { HtmlJsonModel } from "../../model/html-json.model";
+import { HtmlTypeEnum } from "../../enum/html-type.enum";
 
 const htmlExample = `
 <html lang="en">
@@ -10,12 +12,58 @@ const htmlExample = `
 </html>
 `;
 
-describe("HtmlConverterService", () => {
+describe(`HtmlConverterService`, () => {
   const service = container.resolve(HtmlConverterService);
 
-  describe("htmlToJson", () => {
-    test("should be correct when correct html content", () => {
-      expect(service.htmlToJson(htmlExample)).toEqual({});
+  describe(`htmlToJson`, () => {
+    test(`should be correct when correct html content`, () => {
+      expect(service.htmlToJson(htmlExample)).toEqual<HtmlJsonModel[]>([
+        {
+          htmlBase: `<html lang="en">`,
+          htmlType: HtmlTypeEnum.tagOpen,
+          htmlName: `html`,
+          htmlAttributes: {
+            lang: `en`
+          },
+          htmlSelfClose: false,
+          children: [
+            {
+              htmlBase: `<head>`,
+              htmlType: HtmlTypeEnum.tagOpen,
+              htmlName: `head`,
+              htmlAttributes: {},
+              htmlSelfClose: false,
+              children: [
+                {
+                  htmlBase: `<title>`,
+                  htmlType: HtmlTypeEnum.tagOpen,
+                  htmlName: `title`,
+                  htmlAttributes: {},
+                  htmlSelfClose: false,
+                  children: [
+                    {
+                      htmlBase: `Example`,
+                      htmlType: HtmlTypeEnum.tagContent,
+                      htmlName: ``,
+                      htmlAttributes: {},
+                      htmlSelfClose: false,
+                      children: []
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              htmlBase: `<body>`,
+              htmlType: HtmlTypeEnum.tagOpen,
+              htmlName: `body`,
+              htmlAttributes: {},
+              htmlSelfClose: false,
+              children: []
+            }
+          ]
+        }
+      ]);
     });
   });
 });
