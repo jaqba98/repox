16d53c7348e343a3871beb1d74a4cae8dabdfd8b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-08-13 09:18:40 +0200

    feat: create correct loop of loop parser

--- projects/libs/htmlpro/htmlpro-program/src/lib/dom-service/parser/html-json-loop-parser.service.ts
+++ projects/libs/htmlpro/htmlpro-program/src/lib/dom-service/parser/html-json-loop-parser.service.ts
@@ -1,54 +1,80 @@
 import { singleton } from "tsyringe";
 import type {
-  HtmlJsonModel,
-  HtmlJsonAttributeModel
+  HtmlJsonAttributeModel,
+  HtmlJsonModel
 } from "../../model/html-json.model";
 import { HtmlAttributesEnum } from "../../enum/html-attributes.enum";
 import { TypeUtilsService } from "@lib/utils";
+import { HtmlTypeEnum } from "../../enum/html-type.enum";
 
 @singleton()
 /**
  * The service is responsible for parse html json loops.
  */
 export class HtmlJsonLoopParserService {
-  private loopHtmlAttributes: HtmlJsonAttributeModel = {};
-
   constructor(private readonly typeUtils: TypeUtilsService) {
   }
 
   parse(htmlJson: HtmlJsonModel[]): HtmlJsonModel[] {
     return htmlJson
-      .map(html => this.parseChild(html))
+      .map(html => this.parseChild(html, {}))
       .flat();
   }
 
-  private parseChild(htmlJson: HtmlJsonModel): HtmlJsonModel[] {
-    const htmlJsonLoop = this.parseLoop(htmlJson);
-    return htmlJsonLoop.map(htmlJsonImportLoop => ({
-      ...htmlJsonImportLoop,
-      children: htmlJsonImportLoop.children
-        .map(child => this.parseChild(child))
-        .flat()
+  private parseChild(
+    htmlJson: HtmlJsonModel,
+    loopHtmlAttributes: HtmlJsonAttributeModel
+  ): HtmlJsonModel[] {
+    const htmlJsonLoop = this.parseLoop(htmlJson, loopHtmlAttributes);
+    return htmlJsonLoop.map(htmlJsonLoopItem => {
+      return {
+        ...htmlJsonLoopItem.htmlJson,
+        children: htmlJsonLoopItem.htmlJson.children
+          .map(child => this.parseChild(
+            child, htmlJsonLoopItem.loopHtmlAttributes
+          ))
+          .flat()
+      };
+    });
+  }
+
+  private parseLoop(
+    htmlJson: HtmlJsonModel,
+    loopHtmlAttributes: HtmlJsonAttributeModel
+  ): Array<{
+      htmlJson: HtmlJsonModel;
+      loopHtmlAttributes: HtmlJsonAttributeModel;
+    }> {
+    let loop = htmlJson.htmlAttributes[
+      HtmlAttributesEnum.dataLoop
+    ];
+    if (loop === undefined) {
+      return [{ htmlJson, loopHtmlAttributes }];
+    }
+    if (this.typeUtils.valueIsString(loop)) {
+      this.parseAttributes(htmlJson, loopHtmlAttributes);
+      loop = htmlJson.htmlAttributes[HtmlAttributesEnum.dataLoop];
+    }
+    return loop.map((loopItem: any) => ({
+      htmlJson, loopHtmlAttributes: loopItem
     }));
   }
 
-  private parseLoop(htmlJson: HtmlJsonModel): HtmlJsonModel[] {
-    const loop = htmlJson.htmlAttributes[
-      HtmlAttributesEnum.dataLoop
-    ];
-    if (loop === undefined) return [htmlJson];
-    if (this.typeUtils.valueIsString(loop)) {
-      throw new Error(`Dupa`);
+  private parseAttributes(
+    htmlJson: HtmlJsonModel,
+    loopHtmlAttributes: HtmlJsonAttributeModel
+  ): void {
+    if (htmlJson.htmlType === HtmlTypeEnum.tagContent) {
+      return;
     }
-    return loop.map((loopItem: any) => {
-      this.loopHtmlAttributes = loopItem;
-      return {
-        ...htmlJson,
-        htmlAttributes: {
-          ...htmlJson.htmlAttributes,
-          ...loopItem
+    for (const loopHtmlAttribute in loopHtmlAttributes) {
+      for (const htmlAttribute in htmlJson.htmlAttributes) {
+        // eslint-disable-next-line no-useless-escape
+        const pattern: string = `{{\s*${loopHtmlAttribute}\s*}}`;
+        if (new RegExp(pattern, `g`).test(htmlJson.htmlAttributes[htmlAttribute])) {
+          htmlJson.htmlAttributes[htmlAttribute] = loopHtmlAttributes[loopHtmlAttribute];
         }
-      };
-    });
+      }
+    }
   }
 }
