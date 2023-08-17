a9a4d4cd968f3a79c4e6d37df2c0da8f7e004379
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-08-17 19:44:02 +0200

    feat: create top nav for mobile

--- projects/apps/repox-website/src/scripts/hamburger.js
+++ projects/apps/repox-website/src/scripts/hamburger.js
@@ -1,12 +1,12 @@
-// // HTML Elements
-// const hamburgers = document.getElementsByClassName(`hamburger`);
-// const navigations = document.getElementsByClassName(`top-nav`);
-//
-// // Add events
-// Array.from(hamburgers).forEach(hamburger => {
-//   hamburger.addEventListener(`click`, () => {
-//     Array.from(navigations).forEach(nav => {
-//       nav.classList.toggle(`top-nav-open`);
-//     });
-//   });
-// });
+// HTML Elements
+const hamburgers = document.getElementsByClassName(`hamburger`);
+const navigations = document.getElementsByClassName(`top-nav`);
+
+// Add events
+Array.from(hamburgers).forEach(hamburger => {
+  hamburger.addEventListener(`click`, () => {
+    Array.from(navigations).forEach(nav => {
+      nav.classList.toggle(`top-nav-open`);
+    });
+  });
+});
