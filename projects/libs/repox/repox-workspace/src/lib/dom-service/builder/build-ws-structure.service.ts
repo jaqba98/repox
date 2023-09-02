8ce0a5a403cbbf53b56e8bcb298f980fda97a13b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-09-02 11:50:07 +0200

    feat: installing ts-node during generate workspace

--- projects/libs/repox/repox-workspace/src/lib/dom-service/builder/build-ws-structure.service.ts
+++ projects/libs/repox/repox-workspace/src/lib/dom-service/builder/build-ws-structure.service.ts
@@ -138,6 +138,11 @@ export class BuildWsStructureService {
               },
               {
                 type: WsStructureEntityEnum.execCommand,
+                value: `npm i ts-node@10.9.1 -D`,
+                children: []
+              },
+              {
+                type: WsStructureEntityEnum.execCommand,
                 value: `npm i tsc-alias@1.8.7 -D`,
                 children: []
               },
