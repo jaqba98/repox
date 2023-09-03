9e6ebf07bc3344319404520125066dacd7c60770
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-09-03 10:55:58 +0200

    chore: regenerate the workspace

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,34 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
 import type { Config } from "jest";
-import * as path from "path";
+
+interface TsconfigJsonModel {
+  compilerOptions: {
+    paths: Record<string, string[]>;
+  };
+}
+
+function buildModuleNameMapper (): Record<string, string> {
+  const tsconfigText = readFileSync(`tsconfig.json`, `utf-8`);
+  const tsconfigJson = JSON.parse(tsconfigText) as TsconfigJsonModel;
+  if (
+    tsconfigJson === undefined ||
+    tsconfigJson.compilerOptions === undefined ||
+    tsconfigJson.compilerOptions.paths === undefined
+  ) {
+    return {};
+  }
+  const { paths } = tsconfigJson.compilerOptions;
+  return Object.entries(paths)
+    .map(path => ({ alias: path[0], index: path[1][0] }))
+    .reduce((
+      acc: Record<string, string>,
+      curr: { alias: string, index: string }
+    ): Record<string, string> => {
+      acc = { ...acc, [curr.alias]: resolve(__dirname, curr.index) };
+      return acc;
+    }, {});
+}
 
 const config: Config = {
   clearMocks: true,
@@ -7,9 +36,7 @@ const config: Config = {
   preset: `ts-jest`,
   setupFilesAfterEnv: [`core-js/features/reflect`],
   testEnvironment: `jest-environment-node`,
-  moduleNameMapper: {
-    "@lib/const": path.resolve(__dirname, `projects/libs/const/src/index.ts`)
-  }
+  moduleNameMapper: buildModuleNameMapper()
 };
 
 export default config;
