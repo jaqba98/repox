a4a6c89b4ce84d8dc5773f6864e4d2bfe2d89fc4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-02 17:48:06 +0200

    feat: start refactoring the generate project command

--- projects/libs/repox/repox-program/src/lib/program/generate-project-program.service.ts
+++ projects/libs/repox/repox-program/src/lib/program/generate-project-program.service.ts
@@ -1,28 +1,20 @@
-import { singleton } from "tsyringe";
-import { type ProgramModel } from "@lib/model";
-import {
-  GenerateProjectStepService
-} from "../step/generate-project-step.service";
-import {
-  type EmptyRepoxProgramModel,
-  type GenerateProjectRepoxCommandModel
-} from "@lib/repox-domain";
+import {singleton} from "tsyringe";
+import {type ProgramModel} from "@lib/model";
+import {GenerateProjectStepService} from "../step/generate-project-step.service";
+import {type EmptyRepoxProgramModel, type GenerateProjectRepoxCommandModel} from "@lib/repox-domain";
 
 @singleton()
 /**
  * The start point of the program generate project.
  */
-export class GenerateProjectProgramService
-implements ProgramModel {
-  constructor (
-    private readonly step: GenerateProjectStepService
-  ) {
-  }
+export class GenerateProjectProgramService implements ProgramModel {
+    constructor(private readonly step: GenerateProjectStepService) {
+    }
 
-  run (programDomain: unknown, commandDomain: unknown): void {
-    const programModel = programDomain as EmptyRepoxProgramModel;
-    const commandModel = commandDomain as GenerateProjectRepoxCommandModel;
-    this.step.runSteps(programModel, commandModel);
-  }
+    run(programDomain: unknown, commandDomain: unknown): void {
+        this.step.runSteps(
+            programDomain as EmptyRepoxProgramModel,
+            commandDomain as GenerateProjectRepoxCommandModel
+        );
+    }
 }
-// todo: refactor the file
--- projects/libs/repox/repox-program/src/lib/step/generate-project-step.service.ts
+++ projects/libs/repox/repox-program/src/lib/step/generate-project-step.service.ts
@@ -1,78 +1,58 @@
-import { singleton } from "tsyringe";
-import {
-  type EmptyRepoxProgramModel,
-  type GenerateProjectRepoxCommandModel
-} from "@lib/repox-domain";
-import {
-  NewlineAppService,
-  SimpleMessageAppService
-} from "@lib/logger";
-import { REPOX_LOGO } from "@lib/repox-const";
-import { GoToProjectRootAppService } from "@lib/program-step";
-import {
-  LoadWsDtoAppService
-} from "../app-service/load-ws-dto-app.service";
-import {
-  LoadWsDomainAppService
-} from "../app-service/load-ws-domain-app.service";
-import {
-  ProjectNotExistAppService
-} from "../app-service/project-not-exist-app.service";
-import {
-  AddProjectToDomainAppService
-} from "../app-service/add-project-to-domain-app.service";
-import {
-  SaveWsDomainAppService
-} from "../app-service/save-ws-domain-app.service";
-import {
-  SaveWsDtoAppService
-} from "../app-service/save-ws-dto-app.service";
-import {
-  CreateProjectFilesAppService
-} from "../app-service/create-project-files-app.service";
+import {singleton} from "tsyringe";
+import {type EmptyRepoxProgramModel, type GenerateProjectRepoxCommandModel} from "@lib/repox-domain";
+import {NewlineAppService, SimpleMessageAppService} from "@lib/logger";
+import {REPOX_LOGO} from "@lib/repox-const";
+import {GoToProjectRootAppService} from "@lib/program-step";
+import {LoadWsDtoAppService} from "../app-service/load-ws-dto-app.service";
+import {LoadWsDomainAppService} from "../app-service/load-ws-domain-app.service";
+import {ProjectNotExistAppService} from "../app-service/project-not-exist-app.service";
+import {AddProjectToDomainAppService} from "../app-service/add-project-to-domain-app.service";
+import {SaveWsDomainAppService} from "../app-service/save-ws-domain-app.service";
+import {SaveWsDtoAppService} from "../app-service/save-ws-dto-app.service";
+import {CreateProjectFilesAppService} from "../app-service/create-project-files-app.service";
 
 @singleton()
 /**
  * The list of steps for the program generate project.
  */
 export class GenerateProjectStepService {
-  constructor (
-    private readonly simpleMessage: SimpleMessageAppService,
-    private readonly newline: NewlineAppService,
-    // private readonly allProgramInstalled: AllProgramInstalledService,
-    private readonly goToProjectRoot: GoToProjectRootAppService,
-    private readonly loadWsDto: LoadWsDtoAppService,
-    private readonly loadWsDomain: LoadWsDomainAppService,
-    private readonly projectNotExist: ProjectNotExistAppService,
-    private readonly addProjectToDomain: AddProjectToDomainAppService,
-    private readonly saveWsDomain: SaveWsDomainAppService,
-    private readonly saveWsDto: SaveWsDtoAppService,
-    private readonly createProjectFiles: CreateProjectFilesAppService
-  ) {
-  }
+    // todo: I am here
+    constructor(
+        private readonly simpleMessage: SimpleMessageAppService,
+        private readonly newline: NewlineAppService,
+        // private readonly allProgramInstalled: AllProgramInstalledService,
+        private readonly goToProjectRoot: GoToProjectRootAppService,
+        private readonly loadWsDto: LoadWsDtoAppService,
+        private readonly loadWsDomain: LoadWsDomainAppService,
+        private readonly projectNotExist: ProjectNotExistAppService,
+        private readonly addProjectToDomain: AddProjectToDomainAppService,
+        private readonly saveWsDomain: SaveWsDomainAppService,
+        private readonly saveWsDto: SaveWsDtoAppService,
+        private readonly createProjectFiles: CreateProjectFilesAppService
+    ) {
+    }
 
-  runSteps (
-    programModel: EmptyRepoxProgramModel,
-    commandModel: GenerateProjectRepoxCommandModel
-  ): void {
-    this.simpleMessage.writeInfo(`Generate project`, REPOX_LOGO);
-    this.newline.writeNewline();
-    // if (!this.allProgramInstalled.run()) return;
-    if (!this.goToProjectRoot.run()) return;
-    if (!this.loadWsDto.run()) return;
-    if (!this.loadWsDomain.run()) return;
-    const {
-      projectName, projectType, projectPath, projectScheme
-    } = commandModel;
-    if (!this.projectNotExist.run(projectName)) return;
-    this.addProjectToDomain.run(
-      projectName, projectType, projectPath, projectScheme
-    );
-    if (!this.saveWsDomain.run()) return;
-    if (!this.saveWsDto.run()) return;
-    if (!this.createProjectFiles.run(projectName)) return;
-    this.newline.writeNewline();
-    this.simpleMessage.writeSuccess(`Project generated correctly`);
-  }
+    runSteps(
+        programModel: EmptyRepoxProgramModel,
+        commandModel: GenerateProjectRepoxCommandModel
+    ): void {
+        this.simpleMessage.writeInfo(`Generate project`, REPOX_LOGO);
+        this.newline.writeNewline();
+        // if (!this.allProgramInstalled.run()) return;
+        if (!this.goToProjectRoot.run()) return;
+        if (!this.loadWsDto.run()) return;
+        if (!this.loadWsDomain.run()) return;
+        const {
+            projectName, projectType, projectPath, projectScheme
+        } = commandModel;
+        if (!this.projectNotExist.run(projectName)) return;
+        this.addProjectToDomain.run(
+            projectName, projectType, projectPath, projectScheme
+        );
+        if (!this.saveWsDomain.run()) return;
+        if (!this.saveWsDto.run()) return;
+        if (!this.createProjectFiles.run(projectName)) return;
+        this.newline.writeNewline();
+        this.simpleMessage.writeSuccess(`Project generated correctly`);
+    }
 }
-// todo: refactor the file
