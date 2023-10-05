77a9455e933c85be2a728b85680729d850e1cc33
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-05 18:39:25 +0200

    refactor: next steps of refactoring the generate project command

--- projects/libs/repox/repox-program/src/lib/step/generate-project-step.service.ts
+++ projects/libs/repox/repox-program/src/lib/step/generate-project-step.service.ts
@@ -1,58 +1,53 @@
 import {singleton} from "tsyringe";
 import {type EmptyRepoxProgramModel, type GenerateProjectRepoxCommandModel} from "@lib/repox-domain";
-import {NewlineAppService, SimpleMessageAppService} from "@lib/logger";
 import {REPOX_LOGO} from "@lib/repox-const";
-import {GoToProjectRootAppService} from "@lib/program-step";
-import {LoadWsDtoAppService} from "../app-service/load-ws-dto-app.service";
-import {LoadWsDomainAppService} from "../app-service/load-ws-domain-app.service";
-import {ProjectNotExistAppService} from "../app-service/project-not-exist-app.service";
-import {AddProjectToDomainAppService} from "../app-service/add-project-to-domain-app.service";
-import {SaveWsDomainAppService} from "../app-service/save-ws-domain-app.service";
-import {SaveWsDtoAppService} from "../app-service/save-ws-dto-app.service";
-import {CreateProjectFilesAppService} from "../app-service/create-project-files-app.service";
+import {NewlineAppService, SimpleMessageAppService} from "@lib/logger";
+import {SystemProgramEnum, SystemProgramExistAppService} from "@lib/program-step";
 
 @singleton()
 /**
  * The list of steps for the program generate project.
  */
 export class GenerateProjectStepService {
-    // todo: I am here
     constructor(
         private readonly simpleMessage: SimpleMessageAppService,
         private readonly newline: NewlineAppService,
+        private readonly systemProgramExist: SystemProgramExistAppService,
         // private readonly allProgramInstalled: AllProgramInstalledService,
-        private readonly goToProjectRoot: GoToProjectRootAppService,
-        private readonly loadWsDto: LoadWsDtoAppService,
-        private readonly loadWsDomain: LoadWsDomainAppService,
-        private readonly projectNotExist: ProjectNotExistAppService,
-        private readonly addProjectToDomain: AddProjectToDomainAppService,
-        private readonly saveWsDomain: SaveWsDomainAppService,
-        private readonly saveWsDto: SaveWsDtoAppService,
-        private readonly createProjectFiles: CreateProjectFilesAppService
+        // private readonly goToProjectRoot: GoToProjectRootAppService,
+        // private readonly loadWsDto: LoadWsDtoAppService,
+        // private readonly loadWsDomain: LoadWsDomainAppService,
+        // private readonly projectNotExist: ProjectNotExistAppService,
+        // private readonly addProjectToDomain: AddProjectToDomainAppService,
+        // private readonly saveWsDomain: SaveWsDomainAppService,
+        // private readonly saveWsDto: SaveWsDtoAppService,
+        // private readonly createProjectFiles: CreateProjectFilesAppService
     ) {
     }
 
     runSteps(
-        programModel: EmptyRepoxProgramModel,
+        _programModel: EmptyRepoxProgramModel,
         commandModel: GenerateProjectRepoxCommandModel
     ): void {
         this.simpleMessage.writeInfo(`Generate project`, REPOX_LOGO);
         this.newline.writeNewline();
-        // if (!this.allProgramInstalled.run()) return;
-        if (!this.goToProjectRoot.run()) return;
-        if (!this.loadWsDto.run()) return;
-        if (!this.loadWsDomain.run()) return;
-        const {
-            projectName, projectType, projectPath, projectScheme
-        } = commandModel;
-        if (!this.projectNotExist.run(projectName)) return;
-        this.addProjectToDomain.run(
-            projectName, projectType, projectPath, projectScheme
-        );
-        if (!this.saveWsDomain.run()) return;
-        if (!this.saveWsDto.run()) return;
-        if (!this.createProjectFiles.run(projectName)) return;
-        this.newline.writeNewline();
-        this.simpleMessage.writeSuccess(`Project generated correctly`);
+        if (!this.systemProgramExist.run(SystemProgramEnum.node)) return;
+        if (!this.systemProgramExist.run(SystemProgramEnum.npm)) return;
+        if (!this.systemProgramExist.run(SystemProgramEnum.git)) return;
+        const {projectName, projectType, projectPath, projectScheme} = commandModel;
+        console.log(projectName, projectType, projectPath, projectScheme);
+        // todo: I am here
+        // if (!this.goToProjectRoot.run()) return;
+        // if (!this.loadWsDto.run()) return;
+        // if (!this.loadWsDomain.run()) return;
+        // if (!this.projectNotExist.run(projectName)) return;
+        // this.addProjectToDomain.run(
+        //     projectName, projectType, projectPath, projectScheme
+        // );
+        // if (!this.saveWsDomain.run()) return;
+        // if (!this.saveWsDto.run()) return;
+        // if (!this.createProjectFiles.run(projectName)) return;
+        // this.newline.writeNewline();
+        // this.simpleMessage.writeSuccess(`Project generated correctly`);
     }
 }
