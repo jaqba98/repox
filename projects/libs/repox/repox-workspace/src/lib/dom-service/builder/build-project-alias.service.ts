37a993866b5e4ce27e8fe9fd35da098c8ed7d77e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-12 17:55:45 +0200

    chore: update all npm

--- projects/libs/repox/repox-workspace/src/lib/dom-service/builder/build-project-alias.service.ts
+++ projects/libs/repox/repox-workspace/src/lib/dom-service/builder/build-project-alias.service.ts
@@ -1,22 +1,12 @@
-import { singleton } from "tsyringe";
-import {
-  ConvertProjectTypeService
-} from "../converter/convert-project-type.service";
+import {singleton} from "tsyringe";
 
 @singleton()
 /**
  * The service is responsible for create alias for project.
  */
 export class BuildProjectAliasService {
-  constructor (
-    private readonly convertProjectType: ConvertProjectTypeService
-  ) {
-  }
-
-  buildAlias (name: string, type: string | undefined): string {
-    if (type === `app` || type === undefined) return ``;
-    const projectType = this.convertProjectType.toProjectType(type);
-    return `@${projectType}/${name}`;
-  }
+    buildAlias(name: string, type: string | undefined): string {
+        if (type === `app` || type === undefined) return ``;
+        return `@${type}/${name}`;
+    }
 }
-// todo: refactor the file
