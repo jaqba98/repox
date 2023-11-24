38a0bdb27239c17df09add3461db9e00e5d0ff91
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-11-24 12:23:57 +0100

    feat: add verification wheter project is app befor building

--- projects/libs/repox/repox-program/src/lib/app-service/build-project-app.service.ts
+++ projects/libs/repox/repox-program/src/lib/app-service/build-project-app.service.ts
@@ -1,5 +1,6 @@
 import {singleton} from "tsyringe";
 import {SimpleMessageAppService} from "@lib/logger";
+import {ProjectTypeEnum, WsDomainStoreService} from "@lib/repox-workspace";
 
 @singleton()
 /**
@@ -7,102 +8,89 @@ import {SimpleMessageAppService} from "@lib/logger";
  * to the dist folder.
  */
 export class BuildProjectAppService {
-  constructor (
-      private readonly simpleMessage: SimpleMessageAppService
-  ) {
-  }
+    constructor(
+        private readonly simpleMessage: SimpleMessageAppService,
+        private readonly wsDomainStore: WsDomainStoreService,
+    ) {
+    }
 
-  run (_projectName: string): boolean {
-    this.simpleMessage.writePlain(`Step: Build Project`);
-    // this.simpleMessage.writePlain(`Build the project`);
-    // const project = this.wsDomainStore.getProject(projectName);
-    // if (project === undefined) {
-    //   this.simpleMessage.writeError(
-    //     `The ${projectName} project does not exist!`
+    run(projectName: string): boolean {
+        this.simpleMessage.writePlain(`Step: Build Project`);
+        const project = this.wsDomainStore.getProject(projectName);
+        if (project === undefined) {
+            throw new Error(`The project name does not exist in the ws domain`);
+        }
+        if (project.type !== ProjectTypeEnum.appTs) {
+            this.simpleMessage.writeError(`Could not build a project of type ${project.type}.`);
+            this.simpleMessage.writeError(`It it only possible to build applications!`);
+            this.simpleMessage.writeError(`Other types of projects will be build as dependencies!`);
+            return false;
+        }
+        // // if (buildWatch) {
+        // //   this.buildProjectAppTypescript(project);
+        // //   this.buildWatch(project);
+        // //   return true;
+        // // }
+        // this.buildProjectAppTypescript(project);
+        // this.newline.writeNewline();
+        // this.simpleMessage.writeSuccess(`Project built correctly`);
+        return true;
+    }
+
+    // private buildWatch (project: WsProjectDomainModel): void {
+    //   const watcher = watch(
+    //     WorkspaceFolderEnum.projects, { recursive: true }
     //   );
-    //   this.simpleMessage.writeWarning(
-    //     `Specify a different project name and restart the program`
-    //   );
-    //   return false;
+    //   watcher.on(`change`, (_, filename): void => {
+    //     if (this.folderUtils.isFolder(filename.toString())) {
+    //       this.simpleMessage.writePlain(`Rebuilding the project`);
+    //     }
+    //     this.buildProjectAppTypescript(project);
+    //     if (this.folderUtils.isFolder(filename.toString())) {
+    //       this.simpleMessage.writeSuccess(`Project built correctly`);
+    //     }
+    //   });
     // }
-    // if (project.type !== ProjectTypeEnum.appTs) {
-    //   this.simpleMessage.writeError(
-    //     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    //     `Could not build a project of type ${project.type}.`
+
+    // private buildProjectAppTypescript (
+    //   project: WsProjectDomainModel
+    // ): boolean {
+    //   const projectTsconfig = this.pathUtils.createPath(
+    //     project.path, WorkspaceFileEnum.tsconfigJsonFile
     //   );
-    //   this.simpleMessage.writeError(
-    //     `It it only possible to build applications!`
+    //   if (!this.pathUtils.existPath(projectTsconfig)) {
+    //     this.simpleMessage.writeError(
+    //       `There is no tsconfig.json file for the project.`
+    //     );
+    //     return false;
+    //   }
+    //   const projectArg = `--project ${projectTsconfig}`;
+    //   // const distArg = `--outDir ${project.build.output}`;
+    //   const distArg = `--outDir`;
+    //   this.runCommandUtils.runNpxCommand(
+    //     `tsc ${projectArg} ${distArg} --noEmit`, true
     //   );
-    //   this.simpleMessage.writeError(
-    //     `Other types of projects will be build as dependencies!`
+    //   this.runCommandUtils.runNpxCommand(
+    //     `tsc ${projectArg} ${distArg}`, true
     //   );
-    //   return false;
+    //   this.runCommandUtils.runNpxCommand(
+    //     `tsc-alias ${distArg}`, true
+    //   );
+    //   // this.copyAssets(project.build.assets);
+    //   return true;
     // }
-    // // if (buildWatch) {
-    // //   this.buildProjectAppTypescript(project);
-    // //   this.buildWatch(project);
-    // //   return true;
-    // // }
-    // this.buildProjectAppTypescript(project);
-    // this.newline.writeNewline();
-    // this.simpleMessage.writeSuccess(`Project built correctly`);
-    return true;
-  }
 
-  // private buildWatch (project: WsProjectDomainModel): void {
-  //   const watcher = watch(
-  //     WorkspaceFolderEnum.projects, { recursive: true }
-  //   );
-  //   watcher.on(`change`, (_, filename): void => {
-  //     if (this.folderUtils.isFolder(filename.toString())) {
-  //       this.simpleMessage.writePlain(`Rebuilding the project`);
-  //     }
-  //     this.buildProjectAppTypescript(project);
-  //     if (this.folderUtils.isFolder(filename.toString())) {
-  //       this.simpleMessage.writeSuccess(`Project built correctly`);
-  //     }
-  //   });
-  // }
-
-  // private buildProjectAppTypescript (
-  //   project: WsProjectDomainModel
-  // ): boolean {
-  //   const projectTsconfig = this.pathUtils.createPath(
-  //     project.path, WorkspaceFileEnum.tsconfigJsonFile
-  //   );
-  //   if (!this.pathUtils.existPath(projectTsconfig)) {
-  //     this.simpleMessage.writeError(
-  //       `There is no tsconfig.json file for the project.`
-  //     );
-  //     return false;
-  //   }
-  //   const projectArg = `--project ${projectTsconfig}`;
-  //   // const distArg = `--outDir ${project.build.output}`;
-  //   const distArg = `--outDir`;
-  //   this.runCommandUtils.runNpxCommand(
-  //     `tsc ${projectArg} ${distArg} --noEmit`, true
-  //   );
-  //   this.runCommandUtils.runNpxCommand(
-  //     `tsc ${projectArg} ${distArg}`, true
-  //   );
-  //   this.runCommandUtils.runNpxCommand(
-  //     `tsc-alias ${distArg}`, true
-  //   );
-  //   // this.copyAssets(project.build.assets);
-  //   return true;
-  // }
-
-  // private copyAssets (assets: WsAssetsDomainModel[]): void {
-  //   assets
-  //     .map(asset => ({
-  //       output: asset.output,
-  //       files: this.fileUtils.readProjectFiles(asset.input)
-  //     }))
-  //     .map(asset => asset.files.map(file => ({
-  //       file, output: asset.output
-  //     })))
-  //     .flat()
-  //     .forEach(asset => { this.fileUtils.copyFile(asset.file, asset.output); }
-  //     );
-  // }
+    // private copyAssets (assets: WsAssetsDomainModel[]): void {
+    //   assets
+    //     .map(asset => ({
+    //       output: asset.output,
+    //       files: this.fileUtils.readProjectFiles(asset.input)
+    //     }))
+    //     .map(asset => asset.files.map(file => ({
+    //       file, output: asset.output
+    //     })))
+    //     .flat()
+    //     .forEach(asset => { this.fileUtils.copyFile(asset.file, asset.output); }
+    //     );
+    // }
 }
