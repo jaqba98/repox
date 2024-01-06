d3f68bee48450e3b18138796af25d0656be5e67c
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-01-06 14:11:11 +0100

    feat: create basic recursion path

--- projects/libs/repox/repox-workspace/src/lib/app-service/generate-workspace-structure-app.service.ts
+++ projects/libs/repox/repox-workspace/src/lib/app-service/generate-workspace-structure-app.service.ts
@@ -3,31 +3,39 @@ import {singleton} from "tsyringe";
 import {WorkspaceStructureActionsModel} from "../model/workspace/workspace-structure.model";
 import {WORKSPACE_STRUCTURE} from "../const/workspace-structure.const";
 import {WorkspaceActionEnum} from "../enum/workspace/workspace-action.enum";
+import {createPath} from "@lib/utils";
 
 @singleton()
 /**
  * The service uses recursion to generate workspace.
  */
 export class GenerateWorkspaceStructureAppService {
+    private path: string = "";
+
     generate(): boolean {
+        this.path = "";
         return this.runGenerate(WORKSPACE_STRUCTURE.structure);
     }
 
     private runGenerate(actions: WorkspaceStructureActionsModel[]): boolean {
-        // I am here
+        // todo: I am here
         for (const action of actions) {
             switch (action.action) {
                 case WorkspaceActionEnum.createFolder:
-                    console.log(action.folderName);
-                    if (action.subFolders.length > 0) {
-                        this.runGenerate(action.subFolders);
-                    }
+                    this.path = createPath(this.path, action.folderName);
+                    console.log(this.path);
+                    if (action.subFolders.length > 0) this.runGenerate(action.subFolders);
                     break;
                 case WorkspaceActionEnum.createFile:
-                    console.log(action.fileName);
+                    this.path = createPath(this.path, action.fileName);
+                    console.log(this.path);
+                    this.path = createPath(this.path, "../");
                     break;
+                default:
+                    throw new Error("Not supported generate action!");
             }
         }
+        this.path = createPath(this.path, "../");
         return true;
     }
 }
