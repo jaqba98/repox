fed4f702c031b509e6782604525119cf4b3e8341
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-01-06 11:01:36 +0100

    chore: remove the generate folder structure service

--- projects/libs/repox/repox-program/src/lib/app-service/generate-workspace-app.service.ts
+++ projects/libs/repox/repox-program/src/lib/app-service/generate-workspace-app.service.ts
@@ -1,26 +1,17 @@
 import {singleton} from "tsyringe";
 
-import {SimpleMessageAppService, StepMessageAppService} from "@lib/logger";
-import {WorkspaceFolderStructureService} from "@lib/repox-workspace";
+import {StepMessageAppService} from "@lib/logger";
 
 @singleton()
 /**
  * This app service is responsible for generating workspace content.
  */
 export class GenerateWorkspaceAppService {
-    constructor(
-        private readonly stepMessage: StepMessageAppService,
-        private readonly workspaceFolderStructure: WorkspaceFolderStructureService,
-        private readonly simpleMessage: SimpleMessageAppService
-    ) {
+    constructor(private readonly stepMessage: StepMessageAppService) {
     }
 
     run(): boolean {
         this.stepMessage.write("Generate Workspace");
-        if (!this.workspaceFolderStructure.generateStructure()) {
-            this.simpleMessage.writeError("Failed to generate workspace folder structure!");
-            return false;
-        }
         // todo: I am here 1
         return true;
     }
