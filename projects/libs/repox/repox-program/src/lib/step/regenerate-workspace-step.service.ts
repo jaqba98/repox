9f89dffde6836fb542ca01c6758c2418b5558f99
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-01-07 11:19:04 +0100

    refactor: regenerate workspace process

--- projects/libs/repox/repox-program/src/lib/step/regenerate-workspace-step.service.ts
+++ projects/libs/repox/repox-program/src/lib/step/regenerate-workspace-step.service.ts
@@ -3,7 +3,8 @@ import {singleton} from "tsyringe";
 import {NewlineAppService, SimpleMessageAppService} from "@lib/logger";
 import {RegenerateWorkspaceCommandModel} from "@lib/repox-domain";
 import {REPOX_LOGO} from "@lib/repox-const";
-import {CheckForceModeAppService} from "@lib/program-step";
+import {CheckForceModeAppService, GoToWorkspaceRootAppService} from "@lib/program-step";
+import {GenerateWorkspaceAppService} from "../app-service/generate-workspace-app.service";
 
 @singleton()
 /**
@@ -13,7 +14,9 @@ export class RegenerateWorkspaceStepService {
     constructor(
         private readonly simpleMessage: SimpleMessageAppService,
         private readonly newline: NewlineAppService,
-        private readonly checkForceMode: CheckForceModeAppService
+        private readonly checkForceMode: CheckForceModeAppService,
+        private readonly goToWorkspaceRoot: GoToWorkspaceRootAppService,
+        private readonly generateWorkspace: GenerateWorkspaceAppService
     ) {
     }
 
@@ -22,6 +25,11 @@ export class RegenerateWorkspaceStepService {
         this.newline.writeNewline();
         const {isForceMode} = commandModel;
         if (!this.checkForceMode.run(isForceMode)) return;
-        // I am here
+        if (!this.goToWorkspaceRoot.run()) return;
+        if (!this.generateWorkspace.run()) return;
+        this.newline.writeNewline();
+        this.simpleMessage.writeSuccess("Command executed correctly!");
     }
 }
+
+// todo: done
