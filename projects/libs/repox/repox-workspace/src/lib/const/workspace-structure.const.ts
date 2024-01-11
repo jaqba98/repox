11c8cb4fa11712bc0c99a8cdb8aacc7562081d15
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-01-11 23:34:25 +0100

    feat: generate and regenerate gitkeep in empty folder

--- projects/libs/repox/repox-workspace/src/lib/const/workspace-structure.const.ts
+++ projects/libs/repox/repox-workspace/src/lib/const/workspace-structure.const.ts
@@ -5,6 +5,7 @@ import {WorkspaceActionEnum} from "../enum/workspace/workspace-action.enum";
 import {WorkspaceFileEnum} from "../enum/workspace/workspace-file.enum";
 import {BuildGitignoreContentService} from "../dom-service/builder/build-gitignore-content.service";
 import {WorkspaceFolderEnum} from "../enum/workspace/workspace-folder.enum";
+import {BuildGitkeepContentService} from "../dom-service/builder/build-gitkeep-content.service";
 
 /**
  * The contestant contains the whole workspace structure to generate.
@@ -18,17 +19,35 @@ export const WORKSPACE_STRUCTURE: WorkspaceStructureModel = {
                 {
                     action: WorkspaceActionEnum.createFolder,
                     folderName: WorkspaceFolderEnum.apps,
-                    subFolders: []
+                    subFolders: [
+                        {
+                            action: WorkspaceActionEnum.createFile,
+                            fileName: WorkspaceFileEnum.gitKeep,
+                            contentBuilder: container.resolve(BuildGitkeepContentService)
+                        }
+                    ]
                 },
                 {
                     action: WorkspaceActionEnum.createFolder,
                     folderName: WorkspaceFolderEnum.libs,
-                    subFolders: []
+                    subFolders: [
+                        {
+                            action: WorkspaceActionEnum.createFile,
+                            fileName: WorkspaceFileEnum.gitKeep,
+                            contentBuilder: container.resolve(BuildGitkeepContentService)
+                        }
+                    ]
                 },
                 {
                     action: WorkspaceActionEnum.createFolder,
                     folderName: WorkspaceFolderEnum.tools,
-                    subFolders: []
+                    subFolders: [
+                        {
+                            action: WorkspaceActionEnum.createFile,
+                            fileName: WorkspaceFileEnum.gitKeep,
+                            contentBuilder: container.resolve(BuildGitkeepContentService)
+                        }
+                    ]
                 }
             ]
         },
--- projects/libs/repox/repox-workspace/src/lib/dom-service/builder/build-gitkeep-content.service.ts
+++ projects/libs/repox/repox-workspace/src/lib/dom-service/builder/build-gitkeep-content.service.ts
@@ -11,19 +11,25 @@ import {
     pathNotExist,
     removeFile
 } from "@lib/utils";
+import {SystemProgramEnum, SystemProgramExistService} from "@lib/program-step";
 
 @singleton()
 /**
  * The service is responsible for building the .gitkeep content file.
  */
 export class BuildGitkeepContentService implements WorkspaceContentBuilderModel {
+    constructor(private readonly systemProgramExist: SystemProgramExistService) {
+    }
+
     checkBeforeBuildContent(path: string): boolean {
         const parentPath = createParentPath(path);
         if (pathNotExist(parentPath)) return false;
-        if (isEmptyFolder(parentPath)) return true;
+        if (isEmptyFolder(parentPath)) {
+            return this.systemProgramExist.checkExist(SystemProgramEnum.git);
+        }
         if (oneFileInFolder(parentPath) && pathExist(path)) {
             removeFile(path);
-            return true;
+            return this.systemProgramExist.checkExist(SystemProgramEnum.git);
         }
         if (manyFilesInFolder(parentPath) && pathExist(path)) removeFile(path);
         return false;
