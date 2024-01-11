68fecd3c9f837a383043f91e1a441dbce091b147
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-01-11 23:25:11 +0100

    feat: generate and regenerate all folders

--- projects/libs/repox/repox-workspace/src/lib/const/workspace-structure.const.ts
+++ projects/libs/repox/repox-workspace/src/lib/const/workspace-structure.const.ts
@@ -4,12 +4,34 @@ import {WorkspaceStructureModel} from "../model/workspace/workspace-structure.mo
 import {WorkspaceActionEnum} from "../enum/workspace/workspace-action.enum";
 import {WorkspaceFileEnum} from "../enum/workspace/workspace-file.enum";
 import {BuildGitignoreContentService} from "../dom-service/builder/build-gitignore-content.service";
+import {WorkspaceFolderEnum} from "../enum/workspace/workspace-folder.enum";
 
 /**
  * The contestant contains the whole workspace structure to generate.
  */
 export const WORKSPACE_STRUCTURE: WorkspaceStructureModel = {
     structure: [
+        {
+            action: WorkspaceActionEnum.createFolder,
+            folderName: WorkspaceFolderEnum.projects,
+            subFolders: [
+                {
+                    action: WorkspaceActionEnum.createFolder,
+                    folderName: WorkspaceFolderEnum.apps,
+                    subFolders: []
+                },
+                {
+                    action: WorkspaceActionEnum.createFolder,
+                    folderName: WorkspaceFolderEnum.libs,
+                    subFolders: []
+                },
+                {
+                    action: WorkspaceActionEnum.createFolder,
+                    folderName: WorkspaceFolderEnum.tools,
+                    subFolders: []
+                }
+            ]
+        },
         {
             action: WorkspaceActionEnum.createFile,
             fileName: WorkspaceFileEnum.gitignore,
