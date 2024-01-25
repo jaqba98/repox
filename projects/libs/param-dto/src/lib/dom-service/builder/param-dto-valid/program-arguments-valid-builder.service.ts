43075abe70fd49eae1ce0f58a9ef2b3c81ec9716
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-01-25 16:35:11 +0100

    fix: fix the supported signs condition

--- projects/libs/param-dto/src/lib/dom-service/builder/param-dto-valid/program-arguments-valid-builder.service.ts
+++ projects/libs/param-dto/src/lib/dom-service/builder/param-dto-valid/program-arguments-valid-builder.service.ts
@@ -18,7 +18,7 @@ export class ProgramArgumentsValidBuilderService implements ParamDtoValidBuilder
     buildSupportedSignsValid(paramDto: ParamDtoService): ProgramArgumentsValidBuilderService {
         const indexes = paramDto.programArguments
             .filter(argument => argument.baseValue !== "" && argument.index !== -1)
-            .filter(argument => /^[a-zA-Z0-9-="'`,]*$/gm.test(argument.baseValue))
+            .filter(argument => !/^[a-zA-Z0-9-="'`,]*$/gm.test(argument.baseValue))
             .map(argument => argument.index);
         if (indexes.length === 0) return this;
         this.paramDtoValid.supportedSigns = false;
