1938ff6518b22e5005f73810a095eaa43844c9af
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-02-08 19:37:56 +0100

    chore: remove unnecesary comment

--- projects/libs/repox/repox-main/src/lib/repox-main.service.ts
+++ projects/libs/repox/repox-main/src/lib/repox-main.service.ts
@@ -25,32 +25,6 @@ container.resolve(RepoxMainService).run();
 
 // import "core-js/features/reflect";
 // import {container, singleton} from "tsyringe";
-//
-// @singleton()
-// /**
-//  * The main service is responsible for running the repox program.
-//  */
-// export class RepoxMainService {
-//     run(): void {
-//
-//     }
-// }
-//
-// container.resolve(RepoxMainService).run();
-// this.buildParamDto.build();
-// const paramDtoValidation = this.getParamDtoData.getParamDtoValidation();
-// if (!paramDtoValidation.success) {
-//     this.paramErrorMessage.writeParamError(
-//         paramDtoValidation.wrongIndexes,
-//         paramDtoValidation.baseValues,
-//         paramDtoValidation.errors,
-//         paramDtoValidation.tips,
-//         REPOX_LOGO
-//     );
-//     return;
-// }
-// import "core-js/features/reflect";
-// import {container, singleton} from "tsyringe";
 // import {BuildParamDtoAppService, GetParamDtoDataAppService} from "@lib/param-dto";
 // import {ParamErrorMessageAppService} from "@lib/logger";
 // import {BuildParamDomainAppService, ParamDomainAppService} from "@lib/param-domain";
