bd67c9c1ede61f7b3723a25f1ce5a0b4f4f8a65b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-02-12 21:48:00 +0100

    refactor: repox program launcher

--- projects/libs/repox/repox-program/src/lib/repox-program-launcher.ts
+++ projects/libs/repox/repox-program/src/lib/repox-program-launcher.ts
@@ -22,16 +22,41 @@ export class RepoxProgramLauncher {
     }
 
     launchProgram(): boolean {
-        const {program, command} = this.store.get();
-        const action = `${program}-${command}`;
-        switch (action) {
-            case "unknown-unknown":
+        const {program} = this.store.get();
+        switch (program) {
+            case "unknown":
+                return this.launchUnknownCommand();
+            case "generate":
+                return this.launchGenerateCommand();
+            default:
+                this.simpleMessage.writeError(
+                    `The ${program} program does not exist!`
+                );
+                return false;
+        }
+    }
+
+    private launchUnknownCommand(): boolean {
+        const {command} = this.store.get();
+        switch (command) {
+            case "unknown":
                 return this.unknownUnknown.runProgram();
-            case "generate-workspace":
+            default:
+                this.simpleMessage.writeError(
+                    `The ${command} command does not exist for specified program!`
+                );
+                return false;
+        }
+    }
+
+    private launchGenerateCommand(): boolean {
+        const {command} = this.store.get();
+        switch (command) {
+            case "workspace":
                 return this.generateWorkspace.runProgram();
             default:
                 this.simpleMessage.writeError(
-                    `Repox does not support given program: ${program} ${command}`
+                    `The ${command} command does not exist for specified program!`
                 );
                 return false;
         }
