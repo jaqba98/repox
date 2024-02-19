5d8053dc3f94d49a49854e9a14309b4a632f3e52
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-02-19 14:06:44 +0100

    chore: uncomment hte commented code !

--- projects/libs/repox/repox-program/src/lib/program/regenerate-workspace-program.service.ts
+++ projects/libs/repox/repox-program/src/lib/program/regenerate-workspace-program.service.ts
@@ -1,34 +1,34 @@
 import {singleton} from "tsyringe";
-//
-// import {NewlineAppService, SimpleMessageAppService} from "@lib/logger";
-// import {REPOX_LOGO} from "@lib/repox-const";
-// import {ParamDomainStore} from "@lib/param-domain";
-//
-// @singleton()
-// /**
-//  * The start point of the program regenerate, command workspace.
-//  * Possible arguments
-//  */
-// export class RegenerateWorkspaceProgramService {
-//     constructor(
-//         private readonly simpleMessage: SimpleMessageAppService,
-//         private readonly newline: NewlineAppService,
-//         private readonly store: ParamDomainStore
-//     ) {
-//     }
-//
-//     runProgram(): boolean {
-//         this.simpleMessage.writeInfo("Regenerate Workspace", REPOX_LOGO);
-//         this.newline.writeNewline();
-//         const forceMode = this.store.getCommandArg("force", "f");
-//         if (!forceMode) {
-//             this.simpleMessage.writeError("The program must be run in forced mode!");
-//             this.simpleMessage.writeWarning("Specify force mode by --force or -f and rerun the program.");
-//             return false;
-//         }
-//         console.log(forceMode);
-//         return true;
-//     }
-// }
-//
-// // todo: refactor the code
+
+import {NewlineAppService, SimpleMessageAppService} from "@lib/logger";
+import {REPOX_LOGO} from "@lib/repox-const";
+import {ParamDomainStore} from "@lib/param-domain";
+
+@singleton()
+/**
+ * The start point of the program regenerate, command workspace.
+ * Possible arguments
+ */
+export class RegenerateWorkspaceProgramService {
+    constructor(
+        private readonly simpleMessage: SimpleMessageAppService,
+        private readonly newline: NewlineAppService,
+        private readonly store: ParamDomainStore
+    ) {
+    }
+
+    runProgram(): boolean {
+        this.simpleMessage.writeInfo("Regenerate Workspace", REPOX_LOGO);
+        this.newline.writeNewline();
+        const forceMode = this.store.getCommandArg("force", "f");
+        if (!forceMode) {
+            this.simpleMessage.writeError("The program must be run in forced mode!");
+            this.simpleMessage.writeWarning("Specify force mode by --force or -f and rerun the program.");
+            return false;
+        }
+        console.log(forceMode);
+        return true;
+    }
+}
+
+// todo: refactor the code
