5bbc7142179ca1292275d7b888a53a0daec00fad
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-02-29 20:41:05 +0100

    feat: create empty folder for project

--- projects/libs/repox/repox-program/src/lib/program/generate-project-program.service.ts
+++ projects/libs/repox/repox-program/src/lib/program/generate-project-program.service.ts
@@ -2,12 +2,17 @@ import {singleton} from "tsyringe";
 
 import {REPOX_LOGO} from "@lib/repox-const";
 import {NewlineAppService, SimpleMessageAppService} from "@lib/logger";
-import {GoToWorkspaceRootAppService} from "@lib/program-step";
+import {
+    ChangePathAppService,
+    CreateFolderAppService,
+    GoToWorkspaceRootAppService
+} from "@lib/program-step";
 import {ParamDomainStore} from "@lib/param-domain";
 
 import {LoadWorkspaceDtoAppService} from "../app-service/load-workspace-dto-app.service";
 import {SaveWorkspaceDtoAppService} from "../app-service/save-workspace-dto-app.service";
 import {AddProjectToDtoAppService} from "../app-service/add-project-to-dto-app.service";
+import {getCurrentPath} from "@lib/utils";
 
 @singleton()
 /**
@@ -21,6 +26,8 @@ export class GenerateProjectProgramService {
         private readonly goToWorkspaceRoot: GoToWorkspaceRootAppService,
         private readonly loadWorkspaceDto: LoadWorkspaceDtoAppService,
         private readonly addProjectToDto: AddProjectToDtoAppService,
+        private readonly changePath: ChangePathAppService,
+        private readonly createFolder: CreateFolderAppService,
         private readonly saveWorkspaceDto: SaveWorkspaceDtoAppService
     ) {
     }
@@ -28,24 +35,28 @@ export class GenerateProjectProgramService {
     runProgram(): boolean {
         this.simpleMessage.writeInfo("Generate Program", REPOX_LOGO);
         this.newline.writeNewline();
-        const name = this.store.getCommandArg("name", "n");
-        if (!name) {
+        if (!this.goToWorkspaceRoot.run()) return false;
+        const projectNames = this.store.getCommandArg("name", "n");
+        if (!projectNames) {
             this.simpleMessage.writeError("You did not specify a project name!");
             this.simpleMessage.writeWarning("Specify project name by --name or -n and rerun the program.");
             return false;
         }
-        const path = this.store.getCommandArg("path", "p");
-        if (!path) {
+        const projectName = projectNames[0];
+        const projectPaths = this.store.getCommandArg("path", "p");
+        if (!projectPaths) {
             this.simpleMessage.writeError("You did not specify a project path!");
             this.simpleMessage.writeWarning("Specify project path by --path or -p and rerun the program.");
             return false;
         }
-        const projectName = name[0];
-        const projectPath = path[0];
-        if (!this.goToWorkspaceRoot.run()) return false;
+        const projectPath = projectPaths[0];
+        const currentPath = getCurrentPath();
         if (!this.loadWorkspaceDto.run()) return false;
         if (!this.addProjectToDto.run(projectName, projectPath)) return false;
         if (!this.saveWorkspaceDto.run()) return false;
+        if (!this.changePath.run(projectPath)) return false;
+        if (!this.createFolder.run(projectName)) return false;
+        if (!this.changePath.run(currentPath)) return false;
         this.newline.writeNewline();
         this.simpleMessage.writeSuccess("Command executed correctly!");
         return true;
