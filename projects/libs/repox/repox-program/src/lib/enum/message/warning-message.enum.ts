3d66fd2553307b7a75420a7fe026da23d9bd79eb
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-02 00:07:35 +0100

    chore: fix the repox url decs

--- projects/libs/repox/repox-program/src/lib/enum/message/warning-message.enum.ts
+++ projects/libs/repox/repox-program/src/lib/enum/message/warning-message.enum.ts
@@ -2,5 +2,5 @@
  * The enum contains all warning messages for that project.
  */
 export enum WarningMessageEnum {
-    moreInfo = "For more info, take a deeper look through our documentation: https://www.repox.dev/#/docs/api"
+    moreInfo = "For more info, take a deeper look through our documentation: https://www.repox.dev/#/docs"
 }
