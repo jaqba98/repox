78abef560e9d5ea78dc920f5e47d8e0fcca72950
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-14 17:12:18 +0100

    feat: create basic flow of generate project

--- projects/libs/repox/repox-program/src/lib/app-service/program/generate-project-app.service.ts
+++ projects/libs/repox/repox-program/src/lib/app-service/program/generate-project-app.service.ts
@@ -6,6 +6,14 @@ import {CommandEnum} from "../../enum/launcher/command.enum";
 import {
     GetCommandArgSingleValueStep
 } from "../../dom-service/step/get-command-arg-single-value.step";
+import {GoToWorkspaceRootStep} from "../../dom-service/step/go-to-workspace-root.step";
+import {BuildWorkspaceDtoStep} from "../../dom-service/step/build-workspace-dto.step";
+import {
+    BuildWorkspaceDomainStep
+} from "../../dom-service/step/build-workspace-domain.step";
+import {SaveWorkspaceDomainStep} from "../../dom-service/step/save-workspace-domain.step";
+import {SaveWorkspaceDtoStep} from "../../dom-service/step/save-workspace-dto.step";
+import {WriteSuccessStep} from "../../dom-service/step/write-success.step";
 
 @singleton()
 /**
@@ -17,17 +25,12 @@ export class GenerateProjectAppService {
     constructor(
         private readonly writeHeader: WriteHeaderStep,
         private readonly getCommandArgSingleValue: GetCommandArgSingleValueStep,
-        // private readonly systemProgramExist: SystemProgramExistStep,
-        // private readonly runCommand: RunCommandStep,
-        // private readonly folderNotExist: FolderNotExistStep,
-        // private readonly createFolder: CreateFolderStep,
-        // private readonly changePath: ChangePathStep,
-        // private readonly buildWorkspaceDto: BuildWorkspaceDtoStep,
-        // private readonly buildWorkspaceDomain: BuildWorkspaceDomainStep,
-        // private readonly generateWorkspace: GenerateWorkspaceStep,
-        // private readonly saveWorkspaceDomain: SaveWorkspaceDomainStep,
-        // private readonly saveWorkspaceDto: SaveWorkspaceDtoStep,
-        // private readonly writeSuccess: WriteSuccessStep
+        private readonly goToWorkspaceRoot: GoToWorkspaceRootStep,
+        private readonly buildWorkspaceDto: BuildWorkspaceDtoStep,
+        private readonly buildWorkspaceDomain: BuildWorkspaceDomainStep,
+        private readonly saveWorkspaceDomain: SaveWorkspaceDomainStep,
+        private readonly saveWorkspaceDto: SaveWorkspaceDtoStep,
+        private readonly writeSuccess: WriteSuccessStep
     ) {
     }
 
@@ -37,26 +40,12 @@ export class GenerateProjectAppService {
         }
         const name = this.getCommandArgSingleValue.run("name", "n");
         if (!name) return false;
-        console.log(name);
-        // if (!this.systemProgramExist.run(SystemProgramEnum.node)) return false;
-        // if (!this.systemProgramExist.run(SystemProgramEnum.npm)) return false;
-        // if (!this.systemProgramExist.run(SystemProgramEnum.git)) return false;
-        // if (!this.runCommand.run("npm install --global pnpm")) return false;
-        // if (!this.systemProgramExist.run(SystemProgramEnum.pnpm)) return false;
-        // if (!this.folderNotExist.run(name)) return false;
-        // if (!this.createFolder.run(name)) return false;
-        // if (!this.changePath.run(name)) return false;
-        // if (!this.buildWorkspaceDto.run()) return false;
-        // if (!this.buildWorkspaceDomain.run()) return false;
-        // if (!this.generateWorkspace.run()) return false;
-        // if (!this.saveWorkspaceDomain.run()) return false;
-        // if (!this.saveWorkspaceDto.run()) return false;
-        // if (!this.runCommand.run("pnpm install --prefer-offline")) return false;
-        // if (!this.runCommand.run("git init")) return false;
-        // if (!this.runCommand.run("git config core.autocrlf false")) return false;
-        // if (!this.runCommand.run("git add .")) return false;
-        // if (!this.runCommand.run('git commit -m "initial commit"')) return false;
-        // if (!this.writeSuccess.run()) return false;
+        if (!this.goToWorkspaceRoot.run()) return false;
+        if (!this.buildWorkspaceDto.run()) return false;
+        if (!this.buildWorkspaceDomain.run()) return false;
+        if (!this.saveWorkspaceDomain.run()) return false;
+        if (!this.saveWorkspaceDto.run()) return false;
+        if (!this.writeSuccess.run()) return false;
         return true;
     }
 }
