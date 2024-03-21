cf69b8a919cd9ee950ee583b52c185a9a9434558
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-21 17:10:43 +0100

    feat: generate the main.ts file

--- projects/libs/repox/repox-workspace/src/lib/const/app-ts-project-structure.const.ts
+++ projects/libs/repox/repox-workspace/src/lib/const/app-ts-project-structure.const.ts
@@ -3,6 +3,7 @@ import {WorkspaceFolderEnum} from "../enum/workspace-folder.enum";
 import {SrcFolderBuilder} from "../dom-service/builder/src-folder.builder";
 import {AppFolderBuilder} from "../dom-service/builder/app-folder.builder";
 import {ProjectPackageJsonFileBuilder} from "../dom-service/builder/project-package-json-file.builder";
+import {MainTsFileBuilder} from "../dom-service/builder/main-ts-file.builder";
 
 /**
  * The contestant contains the app ts project structure to generate.
@@ -16,7 +17,13 @@ export const APP_TS_PROJECT_STRUCTURE: WorkspaceStructureModel = {
                 {
                     path: WorkspaceFolderEnum.app,
                     builder: AppFolderBuilder,
-                    children: []
+                    children: [
+                        {
+                            path: ".",
+                            builder: MainTsFileBuilder,
+                            children: []
+                        }
+                    ]
                 }
             ]
         },
