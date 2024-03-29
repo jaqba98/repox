e03d5bbc9c6be6dc2993c2af430c8e28c4a33954
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-30 00:02:36 +0100

    refactor: write header step

--- projects/libs/repox/repox-program/src/lib/app-service/program/lint-project-app.service.ts
+++ projects/libs/repox/repox-program/src/lib/app-service/program/lint-project-app.service.ts
@@ -3,43 +3,41 @@ import { singleton } from 'tsyringe'
 import { WriteHeaderStep } from '../../dom-service/step/write-header.step'
 import { ProgramEnum } from '../../enum/launcher/program.enum'
 import { CommandEnum } from '../../enum/launcher/command.enum'
-import { WriteSuccessStep } from '../../dom-service/step/write-success.step'
-import { SystemProgramExistStep } from '../../dom-service/step/system-program-exist.step'
-import { SystemProgramEnum } from '../../enum/system-program/system-program.enum'
-import { GoToWorkspaceRootStep } from '../../dom-service/step/go-to-workspace-root.step'
-import { RunCommandStep } from '../../dom-service/step/run-command.step'
-import { GetCommandArgBooleanValueStep } from '../../dom-service/step/get-command-arg-boolean-value.step'
 
 @singleton()
 /**
- * The app service is responsible for linting projects.
- * Argument | Alias | Description            | Required | Value
- * --all    | -a    | Lint all projects.     | true     | string
+ * The App-Service program is responsible for linting projects.
+ * Argument | Alias | Description | Required | Value
  */
 export class LintProjectAppService {
   constructor (
-    private readonly writeHeader: WriteHeaderStep,
-    private readonly getCommandArgBooleanValue: GetCommandArgBooleanValueStep,
-    private readonly systemProgramExist: SystemProgramExistStep,
-    private readonly goToWorkspaceRoot: GoToWorkspaceRootStep,
-    private readonly runCommand: RunCommandStep,
-    private readonly writeSuccess: WriteSuccessStep
-  ) {
-  }
+    private readonly writeHeader: WriteHeaderStep
+  ) {}
 
   run (): boolean {
-    if (!this.writeHeader.run(ProgramEnum.lint, CommandEnum.project)) {
-      return false
-    }
-    const all = this.getCommandArgBooleanValue.run('all', 'a')
-    if (all == null) return false
-    if (!this.systemProgramExist.run(SystemProgramEnum.node)) return false
-    if (!this.systemProgramExist.run(SystemProgramEnum.npm)) return false
-    if (!this.systemProgramExist.run(SystemProgramEnum.git)) return false
-    if (!this.systemProgramExist.run(SystemProgramEnum.pnpm)) return false
-    if (!this.goToWorkspaceRoot.run()) return false
-    if (!this.runCommand.run('npx eslint **/*.ts --fix', true)) return false
-    if (!this.writeSuccess.run()) return false
+    if (!this.writeHeader.run(ProgramEnum.lint, CommandEnum.project)) return false
     return true
   }
+
+  // constructor (
+  // private readonly getCommandArgBooleanValue: GetCommandArgBooleanValueStep,
+  // private readonly systemProgramExist: SystemProgramExistStep,
+  // private readonly goToWorkspaceRoot: GoToWorkspaceRootStep,
+  // private readonly runCommand: RunCommandStep,
+  // private readonly writeSuccess: WriteSuccessStep
+  // ) {
+  // }
+
+  // run (): boolean {
+  // const all = this.getCommandArgBooleanValue.run('all', 'a')
+  // if (all == null) return false
+  // if (!this.systemProgramExist.run(SystemProgramEnum.node)) return false
+  // if (!this.systemProgramExist.run(SystemProgramEnum.npm)) return false
+  // if (!this.systemProgramExist.run(SystemProgramEnum.git)) return false
+  // if (!this.systemProgramExist.run(SystemProgramEnum.pnpm)) return false
+  // if (!this.goToWorkspaceRoot.run()) return false
+  // if (!this.runCommand.run('npx eslint **/*.ts --fix', true)) return false
+  // if (!this.writeSuccess.run()) return false
+  //   return true
+  // }
 }
--- projects/libs/repox/repox-program/src/lib/dom-service/step/write-header.step.ts
+++ projects/libs/repox/repox-program/src/lib/dom-service/step/write-header.step.ts
@@ -1,11 +1,11 @@
 import { singleton } from 'tsyringe'
 
-import { REPOX_LOGO } from '@lib/repox-const'
-import { NewlineAppService, SimpleMessageAppService } from '@lib/logger'
+import { SimpleMessageAppService, NewlineAppService } from '@lib/logger'
 import { firstLetterUpperCase } from '@lib/utils'
+import { REPOX_LOGO } from '@lib/repox-const'
 
-import { type ProgramEnum } from '../../enum/launcher/program.enum'
 import { type CommandEnum } from '../../enum/launcher/command.enum'
+import { type ProgramEnum } from '../../enum/launcher/program.enum'
 
 @singleton()
 /**
@@ -15,8 +15,7 @@ export class WriteHeaderStep {
   constructor (
     private readonly simpleMessage: SimpleMessageAppService,
     private readonly newline: NewlineAppService
-  ) {
-  }
+  ) {}
 
   run (program: ProgramEnum, command: CommandEnum): boolean {
     const programHeader = firstLetterUpperCase(program)
