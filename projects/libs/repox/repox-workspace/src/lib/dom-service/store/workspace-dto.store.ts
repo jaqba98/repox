c747aa06b637012a53d4b4406202748a8533532f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-04-01 08:02:58 +0200

    fix: wrong import

--- projects/libs/repox/repox-workspace/src/lib/dom-service/store/workspace-dto.store.ts
+++ projects/libs/repox/repox-workspace/src/lib/dom-service/store/workspace-dto.store.ts
@@ -2,7 +2,7 @@
 import { singleton } from 'tsyringe'
 
 import { type PackageJsonDtoPartialModel } from '../../model/dto/package-json-dto.model'
-import { type RepoxJsonDtoPartialModel } from '../../model/dto/repox-json-dto.model'
+import { type PartialRepoxJsonDtoModel } from '../../model/dto/repox-json-dto.model'
 import { type TsconfigJsonDtoPartialModel } from '../../model/dto/tsconfig-json-dto.model'
 
 @singleton()
@@ -12,7 +12,7 @@ import { type TsconfigJsonDtoPartialModel } from '../../model/dto/tsconfig-json-
 export class WorkspaceDtoStore {
   workspacePackageJsonDto: PackageJsonDtoPartialModel | undefined
 
-  repoxJsonDto: RepoxJsonDtoPartialModel | undefined
+  repoxJsonDto: PartialRepoxJsonDtoModel | undefined
 
   tsconfigJsonDto: TsconfigJsonDtoPartialModel | undefined
 }
