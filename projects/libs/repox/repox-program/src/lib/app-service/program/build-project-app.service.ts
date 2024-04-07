384f70fa33cc6ec3032aa352feb77e011e3778e9
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-04-07 15:58:31 +0200

    feat: add build dto, check dto and build domain to the build project program

--- projects/libs/repox/repox-program/src/lib/app-service/program/build-project-app.service.ts
+++ projects/libs/repox/repox-program/src/lib/app-service/program/build-project-app.service.ts
@@ -5,6 +5,10 @@ import { ProgramEnum } from '../../enum/launcher/program.enum';
 import { WriteHeaderStep } from '../../dom-service/step/write-header.step';
 import { WriteSuccessStep } from '../../dom-service/step/write-success.step';
 import { GetCommandArgStringValueStep } from '../../dom-service/step/get-command-arg-string-value.step';
+import { GoToWorkspaceRootStep } from '../../dom-service/step/go-to-workspace-root.step';
+import { BuildWorkspaceDomainStep } from '../../dom-service/step/build-workspace-domain.step';
+import { BuildWorkspaceDtoStep } from '../../dom-service/step/build-workspace-dto.step';
+import { CheckWorkspaceDtoStep } from '../../dom-service/step/check-workspace-dto.step';
 
 @singleton()
 /**
@@ -16,10 +20,10 @@ export class BuildProjectAppService {
   constructor (
     private readonly writeHeader: WriteHeaderStep,
     private readonly getCommandArgStringValue: GetCommandArgStringValueStep,
-    // private readonly goToWorkspaceRoot: GoToWorkspaceRootStep,
-    // private readonly buildWorkspaceDto: BuildWorkspaceDtoStep,
-    // private readonly checkWorkspaceDto: CheckWorkspaceDtoStep,
-    // private readonly buildWorkspaceDomain: BuildWorkspaceDomainStep,
+    private readonly goToWorkspaceRoot: GoToWorkspaceRootStep,
+    private readonly buildWorkspaceDto: BuildWorkspaceDtoStep,
+    private readonly checkWorkspaceDto: CheckWorkspaceDtoStep,
+    private readonly buildWorkspaceDomain: BuildWorkspaceDomainStep,
     // private readonly workspaceDomainStore: WorkspaceDomainStore,
     // private readonly systemProgramExist: SystemProgramExistStep,
     // private readonly projectExist: ProjectExistStep,
@@ -33,10 +37,10 @@ export class BuildProjectAppService {
     if (!this.writeHeader.run(ProgramEnum.build, CommandEnum.project)) return false;
     const name = this.getCommandArgStringValue.run('name', 'n');
     if (name === false) return false;
-    // if (!this.goToWorkspaceRoot.run()) return false;
-    // if (!this.buildWorkspaceDto.run()) return false;
-    // if (!this.checkWorkspaceDto.run()) return false;
-    // if (!this.buildWorkspaceDomain.run()) return false;
+    if (!this.goToWorkspaceRoot.run()) return false;
+    if (!this.buildWorkspaceDto.run()) return false;
+    if (!this.checkWorkspaceDto.run()) return false;
+    if (!this.buildWorkspaceDomain.run()) return false;
     // const workspaceDomain = this.workspaceDomainStore.getWorkspaceDomain();
     // const { packageManager } = workspaceDomain.repoxJsonDomain.defaultOptions;
     // if (!this.systemProgramExist.run(packageManager)) return false;
