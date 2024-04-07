ccafe3c6796821e0b4cb3c39a77ee18b3209efee
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-04-07 13:21:17 +0200

    fix: fix compilation mistakes

--- projects/libs/repox/repox-program/src/lib/app-service/program/generate-workspace-app.service.ts
+++ projects/libs/repox/repox-program/src/lib/app-service/program/generate-workspace-app.service.ts
@@ -3,9 +3,6 @@ import { singleton } from 'tsyringe';
 import { WriteHeaderStep } from '../../dom-service/step/write-header.step';
 import { ProgramEnum } from '../../enum/launcher/program.enum';
 import { CommandEnum } from '../../enum/launcher/command.enum';
-import {
-  GetCommandArgSingleValueStep
-} from '../../dom-service/step/get-command-arg-string-value.step';
 import { SystemProgramExistStep } from '../../dom-service/step/system-program-exist.step';
 import { SystemProgramEnum } from '../../enum/system-program/system-program.enum';
 import { FolderNotExistStep } from '../../dom-service/step/folder-not-exist.step';
@@ -30,7 +27,7 @@ import { RunCommandStep } from '../../dom-service/step/run-command.step';
 export class GenerateWorkspaceAppService {
   constructor (
     private readonly writeHeader: WriteHeaderStep,
-    private readonly getCommandArgSingleValue: GetCommandArgSingleValueStep,
+    // private readonly getCommandArgSingleValue: GetCommandArgSingleValueStep,
     private readonly systemProgramExist: SystemProgramExistStep,
     private readonly runCommand: RunCommandStep,
     private readonly folderNotExist: FolderNotExistStep,
@@ -49,16 +46,16 @@ export class GenerateWorkspaceAppService {
     if (!this.writeHeader.run(ProgramEnum.generate, CommandEnum.workspace)) {
       return false;
     }
-    const name = this.getCommandArgSingleValue.run('name', 'n');
-    if (name == null) return false;
+    // const name = this.getCommandArgSingleValue.run('name', 'n');
+    // if (name == null) return false;
     if (!this.systemProgramExist.run(SystemProgramEnum.node)) return false;
     if (!this.systemProgramExist.run(SystemProgramEnum.npm)) return false;
     if (!this.systemProgramExist.run(SystemProgramEnum.git)) return false;
     if (!this.runCommand.run('npm install --global pnpm')) return false;
     if (!this.systemProgramExist.run(SystemProgramEnum.pnpm)) return false;
-    if (!this.folderNotExist.run(name)) return false;
-    if (!this.createFolder.run(name)) return false;
-    if (!this.changePath.run(name)) return false;
+    if (!this.folderNotExist.run('name')) return false;
+    if (!this.createFolder.run('name')) return false;
+    if (!this.changePath.run('name')) return false;
     if (!this.buildWorkspaceDto.run()) return false;
     if (!this.buildWorkspaceDomain.run()) return false;
     if (!this.generateWorkspace.run()) return false;
