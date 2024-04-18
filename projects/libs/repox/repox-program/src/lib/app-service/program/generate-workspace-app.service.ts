9f9db49adff0d45454bf56c4c01ddeb72ec710f8
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-04-18 17:06:49 +0200

    feat: create the whole content before generate workspace

--- projects/libs/repox/repox-program/src/lib/app-service/program/generate-workspace-app.service.ts
+++ projects/libs/repox/repox-program/src/lib/app-service/program/generate-workspace-app.service.ts
@@ -8,6 +8,8 @@ import { SystemProgramExistStep } from '../../dom-service/step/system-program-ex
 import { SystemProgramEnum } from '../../enum/system-program/system-program.enum';
 import { FolderNotExistStep } from '../../dom-service/step/folder-not-exist.step';
 import { CreateFolderStep } from '../../dom-service/step/create-folder.step';
+import { ChangePathStep } from '../../dom-service/step/change-path.step';
+import { WriteSuccessStep } from '../../dom-service/step/write-success.step';
 
 @singleton()
 /**
@@ -22,13 +24,9 @@ export class GenerateWorkspaceAppService {
     private readonly systemProgramExist: SystemProgramExistStep,
     private readonly folderNotExist: FolderNotExistStep,
     private readonly createFolder: CreateFolderStep,
-    // private readonly changePath: ChangePathStep,
-    // private readonly buildWorkspaceDto: BuildWorkspaceDtoStep,
-    // private readonly buildWorkspaceDomain: BuildWorkspaceDomainStep,
+    private readonly changePath: ChangePathStep,
     // private readonly generateWorkspace: GenerateWorkspaceStep,
-    // private readonly saveWorkspaceDomain: SaveWorkspaceDomainStep,
-    // private readonly saveWorkspaceDto: SaveWorkspaceDtoStep,
-    // private readonly writeSuccess: WriteSuccessStep
+    private readonly writeSuccess: WriteSuccessStep
   ) {
   }
 
@@ -43,18 +41,9 @@ export class GenerateWorkspaceAppService {
     if (!this.systemProgramExist.run(SystemProgramEnum.yarn)) return false;
     if (!this.folderNotExist.run(name)) return false;
     if (!this.createFolder.run(name)) return false;
-    // if (!this.changePath.run('name')) return false;
-    // if (!this.buildWorkspaceDto.run()) return false;
-    // if (!this.buildWorkspaceDomain.run()) return false;
+    if (!this.changePath.run(name)) return false;
     // if (!this.generateWorkspace.run()) return false;
-    // if (!this.saveWorkspaceDomain.run()) return false;
-    // if (!this.saveWorkspaceDto.run()) return false;
-    // if (!this.runCommand.run('pnpm install --prefer-offline')) return false;
-    // if (!this.runCommand.run('git init -b "main"')) return false;
-    // if (!this.runCommand.run('git config core.autocrlf false')) return false;
-    // if (!this.runCommand.run('git add .')) return false;
-    // if (!this.runCommand.run('git commit -m "initial commit"')) return false;
-    // if (!this.writeSuccess.run()) return false;
+    if (!this.writeSuccess.run()) return false;
     return true;
   }
 }
