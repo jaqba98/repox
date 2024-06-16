93d52164b859ab317d59e8fa5a07903f9506c369
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-16 11:43:02 +0200

    feat: create basic launcher test

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export const clearMocks = true;
-export const coverageProvider = "v8";
-export const preset = "ts-jest";
-export const setupFilesAfterEnv = ["core-js/features/reflect"];
-export const testEnvironment = "jest-environment-node";
-export const moduleNameMapper = {};
