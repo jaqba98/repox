b49bddfd80f6bf12d1fb7a46fdd337b76843bdd6
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-17 16:24:09 +0200

    feat: create recursion

--- projects/libs/launcher/src/lib/dom-service/run-actions.service.ts
+++ projects/libs/launcher/src/lib/dom-service/run-actions.service.ts
@@ -1,6 +1,8 @@
 import { container, singleton } from "tsyringe";
 
 import { ActionType } from "../type/action.type";
+import { ActionResultModel } from "../model/action.model";
+import { ActionStatusEnum } from "../enum/action.enum";
 
 @singleton()
 export class RunActionsService {
@@ -8,8 +10,19 @@ export class RunActionsService {
     for (let i = 0; i < actions.length; i++) {
       const action = container.resolve(actions[i]);
       const resultBefore = action.runBefore();
+      if (!this.runResult(resultBefore)) return;
       const resultLogic = action.runLogic();
+      if (!this.runResult(resultLogic)) return;
       const resultAfter = action.runAfter();
+      if (!this.runResult(resultAfter)) return;
     }
+  }
+
+  private runResult(action: ActionResultModel) {
+    if (action.status === ActionStatusEnum.completed) {
+      this.run(action.actions);
+      return true;
+    }
+    return false;
   }
 }
