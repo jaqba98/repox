b58f2544423520100bca330adc158b5a08733844
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-27 17:08:00 +0200

    refactor: logger domain model

--- projects/libs/logger/src/lib/model/logger-domain.model.ts
+++ projects/libs/logger/src/lib/model/logger-domain.model.ts
@@ -1,23 +1,19 @@
 import { StatusEnum } from "@lib/core";
 
 export interface LoggerWordModel {
-  content: string
-  underscore: boolean
-}
-
-export interface LoggerHeaderModel {
-  content: string
-  visible: boolean
+  content: string;
+  underline: boolean;
+  visible: boolean;
 }
 
 export interface LoggerLineModel {
-  status: StatusEnum
-  logo: LoggerHeaderModel
-  header: LoggerHeaderModel
-  words: LoggerWordModel[]
-  newline: number
+  status: StatusEnum;
+  logo: LoggerWordModel;
+  header: LoggerWordModel;
+  words: LoggerWordModel[];
+  newline: number;
 }
 
 export interface LoggerDomainModel {
-  lines: LoggerLineModel[]
+  lines: LoggerLineModel[];
 }
