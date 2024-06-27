309db853f9d693a48cafa9dadaa8a1723bc6c4e4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-27 17:36:52 +0200

    refactor: logger const

---
+++ projects/libs/logger/src/lib/const/header.const.ts
@@ -0,0 +1,16 @@
+import { EMPTY_STRING } from "@lib/core";
+
+export const SUCCESS_HEADER = "SUCCESS";
+export const SUCCESS_ABBREV_HEADER = "SUC";
+
+export const WARNING_HEADER = "WARNING";
+export const WARNING_ABBREV_HEADER = "WRN";
+
+export const ERROR_HEADER = "ERROR";
+export const ERROR_ABBREV_HEADER = "ERR";
+
+export const INFO_HEADER = "INFO";
+export const INFO_ABBREV_HEADER = "INF";
+
+export const DEFAULT_HEADER = EMPTY_STRING;
+export const DEFAULT_ABBREV_HEADER = EMPTY_STRING;
--- projects/libs/logger/src/lib/const/logger.const.ts
+++
@@ -1,6 +0,0 @@
-/**
- * List of general constants of logger.
- */
-
-export const ERR: string = "ERR";
-export const TIP: string = "TIP";
