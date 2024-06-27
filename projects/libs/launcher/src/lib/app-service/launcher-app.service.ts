49320028163bf1d010cc355e6ad22c72a2b16eb6
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-27 23:02:22 +0200

    fix: mistake connected with import app to dom

--- projects/libs/launcher/src/lib/app-service/launcher-app.service.ts
+++ projects/libs/launcher/src/lib/app-service/launcher-app.service.ts
@@ -1,15 +1,45 @@
-import { singleton } from "tsyringe";
+import { container, singleton } from "tsyringe";
 
-import {
-  RunActionsService
-} from "../dom-service/run-actions.service";
 import { ActionType } from "../type/action.type";
+import { EMPTY_STRING, StatusEnum } from "@lib/core";
+import { ActionResultModel } from "../model/action.model";
+import {
+  BuildSimpleMessageService,
+  DEFAULT_HEADER,
+  WriteMessageService
+} from "@lib/logger";
 
 @singleton()
 export class LauncherAppService {
-  constructor(private readonly runActions: RunActionsService) {}
+  constructor(
+    private readonly buildSimpleMessage: BuildSimpleMessageService,
+    private readonly writeMessage: WriteMessageService
+  ) {}
 
   launch(actions: ActionType[]) {
-    this.runActions.run(actions);
+    for (let i = 0; i < actions.length; i++) {
+      const action = container.resolve(actions[i]);
+      const resultBefore = action.runBefore();
+      if (!this.runResult(resultBefore)) return;
+      const resultLogic = action.runLogic();
+      if (!this.runResult(resultLogic)) return;
+      const resultAfter = action.runAfter();
+      if (!this.runResult(resultAfter)) return;
+    }
+  }
+
+  private runResult(action: ActionResultModel) {
+    const message = this.buildSimpleMessage.build(
+      EMPTY_STRING,
+      DEFAULT_HEADER,
+      action.message,
+      StatusEnum.default
+    );
+    this.writeMessage.write(message);
+    if (action.status === StatusEnum.success) {
+      this.launch(action.actions);
+      return true;
+    }
+    return false;
   }
 }
--- projects/libs/logger/src/index.ts
+++ projects/libs/logger/src/index.ts
@@ -1,5 +1,3 @@
-export * from "./lib/app-service/newline-app.service";
-export * from "./lib/app-service/simple-message-app.service";
-export * from "./lib/app-service/step-message-app.service";
 export * from "./lib/dom-service/build-simple-message.service";
 export * from "./lib/const/header.const";
+export * from "./lib/infrastructure/write-message.service";
