1bf25e9ba71dcdf284e0d4580cb0c2a540d2a982
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-01 19:29:06 +0200

    feat: install repox

--- projects/tools/docker/main/deploy-repox-local-down.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ./projects/tools/docker/compose/docker-compose.deploy-repox-local.yaml down --rmi all -v
--- projects/tools/docker/main/deploy-repox-local.sh
+++ projects/tools/docker/main/deploy-repox-local.sh
@@ -3,3 +3,5 @@
 docker build -t system:local -f ./projects/tools/docker/dockerfile/context/Dockerfile.system .
 
 docker-compose -f ./projects/tools/docker/compose/docker-compose.deploy-repox-local.yaml up -d
+
+docker-compose -f ./projects/tools/docker/compose/docker-compose.deploy-repox-local.yaml down --rmi all -v
